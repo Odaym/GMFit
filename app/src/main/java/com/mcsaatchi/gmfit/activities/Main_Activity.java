a4d4608bb9791d96013734372c4206f1a795d024
oday.maleh <oday.m@mcsaatchi.me>
2016-04-07 14:20:04 +0300

    Bottom Bar implementation includes 4 tabs with the appropriate fragment switching for each

--- app/src/main/java/com/mcsaatchi/gmfit/activities/Main_Activity.java
+++ app/src/main/java/com/mcsaatchi/gmfit/activities/Main_Activity.java
@@ -1,28 +1,78 @@
 package com.mcsaatchi.gmfit.activities;
 
 import android.os.Bundle;
+import android.support.annotation.IdRes;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 
 import com.mcsaatchi.gmfit.R;
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnMenuTabClickListener;
 
 public class Main_Activity extends AppCompatActivity {
+
+    private BottomBar bottomBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        bottomBar = BottomBar.attachShy((CoordinatorLayout) findViewById(R.id.myCoordinator),
+                findViewById(R.id.myScrollingContent), savedInstanceState);
+
+        bottomBar.noTopOffset();
+        bottomBar.noNavBarGoodness();
+
+
+        bottomBar.setItemsFromMenu(R.menu.bottom_navigation, new OnMenuTabClickListener() {
+            @Override
+            public void onMenuTabSelected(@IdRes int menuItemId) {
+                Fragment fragmentReplace = null;
+
+                switch (menuItemId) {
+                    case R.id.item_one:
+                        fragmentReplace = new GoogleFit_Fragment();
+                        break;
+                    case R.id.item_two:
+                        fragmentReplace = new Second_Fragment();
+                        break;
+                    case R.id.item_three:
+                        fragmentReplace = new Second_Fragment();
+                        break;
+                    case R.id.item_four:
+                        fragmentReplace = new Second_Fragment();
+                        break;
+                }
+
+                getSupportFragmentManager().beginTransaction().setCustomAnimations(R.anim.fade_in, R.anim.fade_out)
+                        .replace(R.id.fragment_container, fragmentReplace).commit();
+            }
+
+            @Override
+            public void onMenuTabReSelected(@IdRes int menuItemId) {
+            }
+        });
+
+        bottomBar.setDefaultTabPosition(0);
+
+        bottomBar.mapColorForTab(0, ContextCompat.getColor(this, R.color.colorAccent));
+        bottomBar.mapColorForTab(1, 0xFF5D4037);
+        bottomBar.mapColorForTab(2, "#7B1FA2");
+        bottomBar.mapColorForTab(3, "#FF5252");
 
         if (findViewById(R.id.fragment_container) != null) {
             if (savedInstanceState != null) {
                 return;
             }
-
-            GoogleFit_Fragment firstFragment = new GoogleFit_Fragment();
-//            firstFragment.setArguments(getIntent().getExtras());
-
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.fragment_container, firstFragment).commit();
         }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        bottomBar.onSaveInstanceState(outState);
     }
 }
