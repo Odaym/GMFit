96bb972f5d8f7b2019a6fb8645fec1a1ec86aebc
oday.maleh <oday.m@mcsaatchi.me>
2016-04-12 12:17:36 +0300

    remove comments

--- app/src/main/java/com/mcsaatchi/gmfit/activities/Splash_Activity.java
+++ app/src/main/java/com/mcsaatchi/gmfit/activities/Splash_Activity.java
@@ -17,19 +17,11 @@ public class Splash_Activity extends AppCompatActivity {
         int SPLASH_TIME_OUT = 2000;
         new Handler().postDelayed(new Runnable() {
 
-            /*
-             * Showing splash screen with a timer. This will be useful when you
-             * want to show case your app logo / company
-             */
-
             @Override
             public void run() {
-                // This method will be executed once the timer is over
-                // Start your app main activity
-                Intent i = new Intent(Splash_Activity.this, Main_Activity.class);
+                Intent i = new Intent(Splash_Activity.this, Login_Activity.class);
                 startActivity(i);
 
-                // close this activity
                 finish();
             }
         }, SPLASH_TIME_OUT);
