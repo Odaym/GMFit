b28da6b796fd4c42c6da295d5f5de892eea07e51
oday.maleh <oday.m@mcsaatchi.me>
2016-04-12 16:44:04 +0300

    Set a back button on activities that inherit from this one

--- app/src/main/java/com/mcsaatchi/gmfit/activities/Base_Activity.java
+++ app/src/main/java/com/mcsaatchi/gmfit/activities/Base_Activity.java
@@ -1,11 +1,21 @@
 package com.mcsaatchi.gmfit.activities;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
 import android.view.MenuItem;
 
 public class Base_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getSupportActionBar() != null)
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
