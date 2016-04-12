04b531bc1c2f7dc5615a4fef355ea5baff86fb14
oday.maleh <oday.m@mcsaatchi.me>
2016-04-12 12:18:18 +0300

    The indicator and its controller, appearing at the bottom of the IntroSlider_Fragment layout for visual switching dots

---
+++ app/src/main/java/com/mcsaatchi/gmfit/classes/Indicator_Controller.java
@@ -0,0 +1,17 @@
+package com.mcsaatchi.gmfit.classes;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.view.View;
+
+public interface Indicator_Controller {
+    View newInstance(@NonNull Context context);
+
+    void initialize(int slideCount);
+
+    void selectPosition(int index);
+
+    void setSelectedIndicatorColor(int color);
+
+    void setUnselectedIndicatorColor(int color);
+}
