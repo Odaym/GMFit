ed2463ec016a9bc7c29c4fdbfc7288c00d201ae0
oday.maleh <oday.m@mcsaatchi.me>
2016-04-13 14:52:51 +0300

    Terms of Usage Activity implmentation and the strings associated with it.

---
+++ app/src/main/java/com/mcsaatchi/gmfit/activities/TOS_Activity.java
@@ -0,0 +1,30 @@
+package com.mcsaatchi.gmfit.activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
+
+import com.mcsaatchi.gmfit.R;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class TOS_Activity extends Base_Activity {
+    @Bind(R.id.termsOfUseTV)
+    TextView termsOfUseTV;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        Bundle bundle = new Bundle();
+        bundle.putInt("activity_title", R.string.tos_activity_title);
+
+        super.onCreate(bundle);
+
+        setContentView(R.layout.tos_activity);
+
+        ButterKnife.bind(this);
+
+        termsOfUseTV.setMovementMethod(LinkMovementMethod.getInstance());
+    }
+}
