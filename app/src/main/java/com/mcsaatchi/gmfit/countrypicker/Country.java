920308bf7334208f9a270dedf18c0826aa06507c
oday.maleh <oday.m@mcsaatchi.me>
2016-04-14 12:36:03 +0300

    Huge Countries Picker implementation to be used in Setting Up user profile (step 1)

---
+++ app/src/main/java/com/mcsaatchi/gmfit/countrypicker/Country.java
@@ -0,0 +1,23 @@
+package com.mcsaatchi.gmfit.countrypicker;
+
+public class Country {
+	private String code;
+	private String name;
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
---
+++ app/src/main/java/com/mcsaatchi/gmfit/countrypicker/CountryPickerListener.java
@@ -0,0 +1,9 @@
+package com.mcsaatchi.gmfit.countrypicker;
+
+/**
+ * Inform the client which country has been selected
+ *
+ */
+public interface CountryPickerListener {
+	public void onSelectCountry(String name, String code);
+}
