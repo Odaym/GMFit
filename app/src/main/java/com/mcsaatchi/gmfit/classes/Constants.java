7b89c30fe9acec8ff72ff7e58972624a9149448b
oday.maleh <oday.m@mcsaatchi.me>
2016-04-22 12:42:50 +0300

    Some Constants for createActivityBundleWithProperties

--- app/src/main/java/com/mcsaatchi/gmfit/classes/Constants.java
+++ app/src/main/java/com/mcsaatchi/gmfit/classes/Constants.java
@@ -6,4 +6,8 @@ public class Constants {
     public static final String EXTRAS_USER_FULL_NAME = "user_full_name";
     public static final String EXTRAS_USER_EMAIL = "user_email_address";
     public static final String EXTRAS_USER_DISPLAY_PHOTO = "user_display_photo";
+
+    public static final String BUNDLE_ACTIVITY_TITLE = "activity_title";
+    public static final String BUNDLE_ACTIVITY_BACK_BUTTON_ENABLED = "activity_back_button_enabled";
+
 }
