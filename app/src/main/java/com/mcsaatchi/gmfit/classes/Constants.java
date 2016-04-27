e5cc6a245a07329f1b0e873e674b3c64fe0c0a78
oday.maleh <oday.m@mcsaatchi.me>
2016-04-27 17:00:19 +0300

    Some constants for referring to the array of widgets and its order and the name of the order-changed event

--- app/src/main/java/com/mcsaatchi/gmfit/classes/Constants.java
+++ app/src/main/java/com/mcsaatchi/gmfit/classes/Constants.java
@@ -2,6 +2,11 @@ package com.mcsaatchi.gmfit.classes;
 
 public class Constants {
     public static final String EXTRAS_PREFS = "GMFIT_PREFS";
+    public static final String TAG = "GMFIT_DEBUG";
+
+    public static final String EXTRAS_WIDGETS_ORDER_ARRAY = "widgets_order_array";
+    public static final String EVENT_WIDGETS_ORDER_ARRAY_CHANGED = "widgets_order_array_changed";
+
     public static final String EXTRAS_USER_LOGGED_IN = "user_logged_in";
     public static final String EXTRAS_USER_FULL_NAME = "user_full_name";
     public static final String EXTRAS_USER_EMAIL = "user_email_address";
@@ -9,5 +14,4 @@ public class Constants {
 
     public static final String BUNDLE_ACTIVITY_TITLE = "activity_title";
     public static final String BUNDLE_ACTIVITY_BACK_BUTTON_ENABLED = "activity_back_button_enabled";
-
 }
