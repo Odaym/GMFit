3255e332b07c65f2836b796abac0a33c7712fb5d
oday.maleh <oday.m@mcsaatchi.me>
2016-05-18 16:42:13 +0300

    Whenever Add is pressed on each Meal segment, open the Add Meal Item Activity. When the result is chosen from the list, figure out which section it belongs to and add it there

--- app/src/main/java/com/mcsaatchi/gmfit/fragments/Nutrition_Fragment.java
+++ app/src/main/java/com/mcsaatchi/gmfit/fragments/Nutrition_Fragment.java
@@ -1,5 +1,6 @@
 package com.mcsaatchi.gmfit.fragments;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -21,6 +22,8 @@ import com.mcsaatchi.gmfit.R;
 import com.mcsaatchi.gmfit.activities.AddNewChart_Activity;
 import com.mcsaatchi.gmfit.activities.AddNewMealItem_Activity;
 import com.mcsaatchi.gmfit.classes.Constants;
+import com.mcsaatchi.gmfit.classes.EventBus_Poster;
+import com.mcsaatchi.gmfit.classes.EventBus_Singleton;
 import com.mcsaatchi.gmfit.classes.Helpers;
 
 import butterknife.Bind;
@@ -29,15 +32,55 @@ import butterknife.ButterKnife;
 public class Nutrition_Fragment extends Fragment {
 
     private NestedScrollView parentScrollView;
+    private Activity parentActivity;
 
-    @Bind(R.id.dynamicArcView)
-    DecoView dynamicArc;
+    /**
+     * CHARTS
+     */
     @Bind(R.id.cards_container)
     LinearLayout cards_container;
+
+    /**
+     * BREAKFAST CHART
+     */
+    @Bind(R.id.chartTitleTV_BREAKFAST)
+    TextView chartTitleTV_BREAKFAST;
+    @Bind(R.id.addEntryBTN_BREAKFAST)
+    Button addNewEntryBTN_BREAKFAST;
+    @Bind(R.id.entriesContainerLayout_BREAKFAST)
+    LinearLayout entriesContainerLayout_BREAKFAST;
+
+    /**
+     * LUNCH CHART
+     */
+    @Bind(R.id.chartTitleTV_LUNCH)
+    TextView chartTitleTV_LUNCH;
+    @Bind(R.id.addEntryBTN_LUNCH)
+    Button addNewEntryBTN_LUNCH;
+    @Bind(R.id.entriesContainerLayout_LUNCH)
+    LinearLayout entriesContainerLayout_LUNCH;
+
+    /**
+     * DINNER CHART
+     */
+    @Bind(R.id.chartTitleTV_DINNER)
+    TextView chartTitleTV_DINNER;
+    @Bind(R.id.addEntryBTN_DINNER)
+    Button addNewEntryBTN_DINNER;
+    @Bind(R.id.entriesContainerLayout_DINNER)
+    LinearLayout entriesContainerLayout_DINNER;
+
+    /**
+     * ADD CHART BUTTON
+     */
     @Bind(R.id.addChartBTN)
     Button addNewChartBTN;
-    @Bind(R.id.chartTitleTV)
-    TextView chartTitleTV;
+
+    /**
+     * TOP LAYOUT WITH WIDGETS
+     */
+    @Bind(R.id.dynamicArcView)
+    DecoView dynamicArc;
 
     @Bind(R.id.firstMetricTitleTV)
     TextView firstMetricTitleTV;
@@ -67,9 +110,6 @@ public class Nutrition_Fragment extends Fragment {
     @Bind(R.id.fourthMetricPercentagesTV)
     TextView fourthMetricPercentagesTV;
 
-    @Bind(R.id.addEntryBTN)
-    Button addNewEntryBTN;
-
     private SparseArray<String[]> itemsMap;
 
     public static final int ADD_NEW_NUTRITION_CHART_REQUEST = 2;
@@ -93,6 +133,8 @@ public class Nutrition_Fragment extends Fragment {
 
         View fragmentView = inflater.inflate(R.layout.fragment_nutrition, container, false);
 
+        EventBus_Singleton.getInstance().register(this);
+
         parentScrollView = (NestedScrollView) getActivity().findViewById(R.id.myScrollingContent);
 
         ButterKnife.bind(this, fragmentView);
@@ -113,14 +155,61 @@ public class Nutrition_Fragment extends Fragment {
             }
         });
 
-        addNewEntryBTN.setOnClickListener(new View.OnClickListener() {
+        addNewEntryBTN_BREAKFAST.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                addNewNutritionElement(chartTitleTV.getText().toString());
+                openMealEntryPickerActivity(chartTitleTV_BREAKFAST.getText().toString());
+            }
+        });
+
+
+        addNewEntryBTN_LUNCH.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMealEntryPickerActivity(chartTitleTV_LUNCH.getText().toString());
+            }
+        });
+
+
+        addNewEntryBTN_DINNER.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMealEntryPickerActivity(chartTitleTV_DINNER.getText().toString());
             }
         });
 
         return fragmentView;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        parentActivity = (Activity) context;
+    }
+
+    @com.squareup.otto.Subscribe
+    public void handle_BusEvents(EventBus_Poster ebp) {
+        String ebpMessage = ebp.getMessage();
+
+        switch (ebpMessage) {
+            case Constants.EXTRAS_PICKED_MEAL_ENTRY:
+                if (ebp.getMealItemExtra() != null) {
+                    switch(ebp.getMealItemExtra().getType()){
+                        case "BREAKFAST":
+                            addMealEntryLayout(ebp.getMealItemExtra().getName(), entriesContainerLayout_BREAKFAST);
+                            break;
+                        case "LUNCH":
+                            addMealEntryLayout(ebp.getMealItemExtra().getName(), entriesContainerLayout_LUNCH);
+                            break;
+                        case "DINNER":
+                            addMealEntryLayout(ebp.getMealItemExtra().getName(), entriesContainerLayout_DINNER);
+                            break;
+                    }
+                }
+
+                break;
+        }
     }
 
     @Override
@@ -136,14 +225,29 @@ public class Nutrition_Fragment extends Fragment {
         }
     }
 
-    private void addNewNutritionElement(String mainMealName) {
-        Intent intent = new Intent(getActivity(), AddNewMealItem_Activity.class);
+    private void openMealEntryPickerActivity(String mainMealName) {
+        Intent intent = new Intent(parentActivity, AddNewMealItem_Activity.class);
         intent.putExtra(Constants.EXTRAS_MAIN_MEAL_NAME, mainMealName);
         startActivity(intent);
     }
 
+    private void addMealEntryLayout(String mealTitle, LinearLayout targetLayoutForMeal) {
+        final View mealEntryLayout = parentActivity.getLayoutInflater().inflate(R.layout.view_generic_chart_entry, null);
+
+        TextView entryTitleTV = (TextView) mealEntryLayout.findViewById(R.id.entryTitleTV);
+        TextView entryDescription = (TextView) mealEntryLayout.findViewById(R.id.entryDescriptionTV);
+        TextView entryUnitsTV = (TextView) mealEntryLayout.findViewById(R.id.entryUnitsTV);
+
+        entryTitleTV.setText(mealTitle);
+        entryDescription.setText("This is a description");
+        entryUnitsTV.setText("400 mg");
+
+        mealEntryLayout.setPadding(0, 0, 0, 30);
+        targetLayoutForMeal.addView(mealEntryLayout);
+    }
+
     private void addNewBarChart(String chartTitle) {
-        final View barChartLayout = getActivity().getLayoutInflater().inflate(R.layout.view_barchart_container, null);
+        final View barChartLayout = parentActivity.getLayoutInflater().inflate(R.layout.view_barchart_container, null);
 
         Button removeChartBTN = (Button) barChartLayout.findViewById(R.id.removeChartBTN);
         final CardView cardLayout = (CardView) barChartLayout.findViewById(R.id.cardLayoutContainer);
