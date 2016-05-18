78aaabc7f77af58aca7c4fe0b46a2f5c370ab1c0
oday.maleh <oday.m@mcsaatchi.me>
2016-05-18 09:14:30 +0300

    Better conditional for handling when a String or a String Resource ID is passed to this onCreate function so that activities can send either

--- app/src/main/java/com/mcsaatchi/gmfit/activities/Base_Activity.java
+++ app/src/main/java/com/mcsaatchi/gmfit/activities/Base_Activity.java
@@ -6,29 +6,36 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
 import android.view.MenuItem;
 
+import com.j256.ormlite.android.apptools.OpenHelperManager;
 import com.mcsaatchi.gmfit.R;
 import com.mcsaatchi.gmfit.classes.Constants;
+import com.mcsaatchi.gmfit.classes.DBHelper;
 
 public class Base_Activity extends AppCompatActivity {
+    private DBHelper dbHelper = null;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         if (getSupportActionBar() != null && savedInstanceState != null) {
-            if (savedInstanceState.get(Constants.BUNDLE_ACTIVITY_TITLE) != null)
-                if (savedInstanceState.getInt(Constants.BUNDLE_ACTIVITY_TITLE, -1) != -1)
-                    getSupportActionBar().setTitle(getString(savedInstanceState.getInt(Constants.BUNDLE_ACTIVITY_TITLE)));
-                else
-                    getSupportActionBar().setTitle(savedInstanceState.getString(Constants.BUNDLE_ACTIVITY_TITLE));
-            else
+            if (savedInstanceState.get(Constants.BUNDLE_ACTIVITY_TITLE) != null && savedInstanceState.get(Constants.BUNDLE_ACTIVITY_TITLE) instanceof String) {
+                //String was passed
+                getSupportActionBar().setTitle(savedInstanceState.getString(Constants.BUNDLE_ACTIVITY_TITLE));
+            } else if (savedInstanceState.get(Constants.BUNDLE_ACTIVITY_TITLE) != null) {
+                //Integer was passed
+                getSupportActionBar().setTitle(getString(savedInstanceState.getInt(Constants.BUNDLE_ACTIVITY_TITLE)));
+            } else {
+                //Null was passed
                 getSupportActionBar().setTitle(getString(R.string.app_name));
+            }
 
             //Set back button only on applicable activities
             if (savedInstanceState.get(Constants.BUNDLE_ACTIVITY_BACK_BUTTON_ENABLED) != null && savedInstanceState.getBoolean(Constants.BUNDLE_ACTIVITY_BACK_BUTTON_ENABLED)) {
                 getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             }
         }
+
     }
 
     @Override
@@ -55,5 +62,21 @@ public class Base_Activity extends AppCompatActivity {
             return true;
         }
         return super.onKeyDown(keyCode, event);
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (dbHelper != null) {
+            OpenHelperManager.releaseHelper();
+            dbHelper = null;
+        }
+    }
+
+    public DBHelper getHelper() {
+        if (dbHelper == null) {
+            dbHelper =
+                    OpenHelperManager.getHelper(this, DBHelper.class);
+        }
+        return dbHelper;
     }
 }
