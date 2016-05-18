166cfe4dd773758a171507d3b9e4a8d7e305703d
oday.maleh <oday.m@mcsaatchi.me>
2016-05-18 16:37:07 +0300

    Extending the EventBus Poster to accept MealItem in the event payload

--- app/src/main/java/com/mcsaatchi/gmfit/classes/EventBus_Poster.java
+++ app/src/main/java/com/mcsaatchi/gmfit/classes/EventBus_Poster.java
@@ -2,8 +2,12 @@ package com.mcsaatchi.gmfit.classes;
 
 import android.util.SparseArray;
 
+import com.mcsaatchi.gmfit.models.MealItem;
+
 public class EventBus_Poster {
     private String message;
+    private String stringExtra;
+    private MealItem mealItem;
     private SparseArray<String[]> sparseArrayExtra;
 
     public EventBus_Poster(String message) {
@@ -13,6 +17,28 @@ public class EventBus_Poster {
     public EventBus_Poster(String message, SparseArray<String[]> sparseArrayExtra) {
         this.message = message;
         this.sparseArrayExtra = sparseArrayExtra;
+    }
+
+    public EventBus_Poster(String message, MealItem mealItem) {
+        this.message = message;
+        this.mealItem = mealItem;
+    }
+
+    public EventBus_Poster(String message, String stringExtra) {
+        this.message = message;
+        this.stringExtra = stringExtra;
+    }
+
+    public MealItem getMealItemExtra(){
+        return mealItem;
+    }
+
+    public String getStringExtra() {
+        return stringExtra;
+    }
+
+    public void setStringExtra(String stringExtra) {
+        this.stringExtra = stringExtra;
     }
 
     public String getMessage() {
