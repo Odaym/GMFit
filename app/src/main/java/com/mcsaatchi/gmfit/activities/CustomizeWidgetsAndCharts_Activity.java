b1fe6abf43fd7087eabdb4d2e9b5d04188bb20d7
oday.maleh <oday.m@mcsaatchi.me>
2016-05-20 17:14:49 +0300

    Activity hosting a viewpager that switches between Customize Charts and Customize Widgets out in the main Fragments for Fitness, Nutrition and Health

---
+++ app/src/main/java/com/mcsaatchi/gmfit/activities/CustomizeWidgetsAndCharts_Activity.java
@@ -0,0 +1,95 @@
+package com.mcsaatchi.gmfit.activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+
+import com.mcsaatchi.gmfit.R;
+import com.mcsaatchi.gmfit.classes.Constants;
+import com.mcsaatchi.gmfit.classes.Helpers;
+import com.mcsaatchi.gmfit.classes.SlidingTabLayout;
+import com.mcsaatchi.gmfit.fragments.CustomizeWidget_Fragment;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class CustomizeWidgetsAndCharts_Activity extends Base_Activity {
+    @Bind(R.id.pager)
+    ViewPager pager;
+    @Bind(R.id.tabs)
+    SlidingTabLayout tabs;
+
+    private String typeOfFragmentToCustomizeFor;
+
+    private String[] tabTitles = new String[]{
+            "Widgets", "Charts"
+    };
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(Helpers.createActivityBundleWithProperties(R.string.customize_widgets_and_charts_activity_title, true));
+
+        setContentView(R.layout.activity_customize_widgets_and_charts);
+
+        ButterKnife.bind(this);
+
+        CustomizerViewPager_Adapter fragmentsPagerAdapter = new CustomizerViewPager_Adapter(getSupportFragmentManager(), tabTitles);
+
+        tabs.setDistributeEvenly(true);
+
+        pager.setAdapter(fragmentsPagerAdapter);
+        pager.setCurrentItem(0);
+
+        tabs.setViewPager(pager);
+
+        Bundle intentExtras = getIntent().getExtras();
+
+        //Grab the Fragment type from one of the three Fragments (Fitness, Nutrition, Health)
+        if (intentExtras != null) {
+            typeOfFragmentToCustomizeFor = intentExtras.getString(Constants.EXTRAS_CUSTOMIZE_WIDGETS_FRAGMENT_TYPE);
+        }
+    }
+
+    public class CustomizerViewPager_Adapter extends FragmentStatePagerAdapter {
+
+        private String[] tabTitles;
+
+        public CustomizerViewPager_Adapter(FragmentManager fm, String[] tabTitles) {
+            super(fm);
+
+            this.tabTitles = tabTitles;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            Bundle fragmentArguments = new Bundle();
+            fragmentArguments.putString(Constants.EXTRAS_CUSTOMIZE_WIDGETS_FRAGMENT_TYPE, typeOfFragmentToCustomizeFor);
+
+            switch (position) {
+                case 0:
+                    Fragment customizeWidgetFragment = new CustomizeWidget_Fragment();
+                    customizeWidgetFragment.setArguments(fragmentArguments);
+                    return customizeWidgetFragment;
+                case 1:
+                    Fragment customizeChartsFragment = new CustomizeWidget_Fragment();
+                    customizeChartsFragment.setArguments(fragmentArguments);
+                    return customizeChartsFragment;
+            }
+
+            return null;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return tabTitles[position];
+        }
+
+        @Override
+        public int getCount() {
+            return tabTitles.length;
+        }
+    }
+}
