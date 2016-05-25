77b8262c395fb86895837724215625f286845ab6
oday.maleh <oday.m@mcsaatchi.me>
2016-05-25 16:37:30 +0300

    Some new constants for use within the activities/fragments for changing order of widgets and charts according to where the user is coming from (which fragment)

--- app/src/main/java/com/mcsaatchi/gmfit/classes/Cons.java
+++ app/src/main/java/com/mcsaatchi/gmfit/classes/Cons.java
@@ -4,6 +4,11 @@ public class Cons {
     public static final String EXTRAS_PREFS = "GMFIT_PREFS";
     public static final String TAG = "GMFIT_DEBUG";
 
+    public static final String EXTRAS_FITNESS_FRAGMENT = "FITNESS";
+    public static final String EXTRAS_NUTRITION_FRAGMENT = "NUTRITION";
+    public static final String EXTRAS_HEALTH_FRAGMENT = "HEALTH";
+
+
     public static final String EXTRAS_ADD_CHART_WHAT_TYPE = "add_chart_what_type";
     public static final String EXTRAS_CHART_TYPE_SELECTED = "chart_type_selected";
 
@@ -34,17 +39,6 @@ public class Cons {
     }
 
     public static final int BarChart_CHART_TYPE = 1;
-    public static final int BarLineChartBase_CHART_TYPE = 2;
-    public static final int BubbleChart_CHART_TYPE = 3;
-    public static final int CandleStickChart_CHART_TYPE = 4;
-    public static final int Chart_CHART_TYPE = 5;
-    public static final int CombinedChart_CHART_TYPE = 6;
-    public static final int HorizontalBarChart_CHART_TYPE = 7;
-    public static final int LineChart_CHART_TYPE = 8;
-    public static final int PieChart_CHART_TYPE = 9;
-    public static final int PieRadarChartBase_CHART_TYPE = 10;
-    public static final int RadarChart_CHART_TYPE = 11;
-    public static final int ScatterChart_CHART_TYPE = 12;
 
     public static final String EXTRAS_FITNESS_WIDGETS_ORDER_ARRAY = "fitness_widgets_order_array";
     public static final String EXTRAS_FITNESS_WIDGETS_ORDER_ARRAY_CHANGED = "fitness_widgets_order_array_changed";
@@ -53,10 +47,12 @@ public class Cons {
     public static final String EXTRAS_HEALTH_WIDGETS_ORDER_ARRAY = "health_widgets_order_array";
     public static final String EXTRAS_HEALTH_WIDGETS_ORDER_ARRAY_CHANGED = "health_widgets_order_array_changed";
 
+    public static final String EXTRAS_FITNESS_CHARTS_ORDER_ARRAY_CHANGED = "fitness_charts_order_array_changed";
+    public static final String EXTRAS_NUTRITION_CHARTS_ORDER_ARRAY_CHANGED = "nutrition_charts_order_array_changed";
+    public static final String EXTRAS_HEALTH_CHARTS_ORDER_ARRAY_CHANGED = "health_charts_order_array_changed";
+
     public static final String EXTRAS_CUSTOMIZE_WIDGETS_FRAGMENT_TYPE = "customize_widgets_type";
-    public static final String EXTRAS_FITNESS_FRAGMENT_IDENTIFIER = "FITNESS";
-    public static final String EXTRAS_NUTRITION_FRAGMENT_IDENTIFIER = "NUTRITION";
-    public static final String EXTRAS_HEALTH_FRAGMENT_IDENTIFIER = "HEALTH";
+    public static final String EXTRAS_ALL_DATA_CHARTS = "all_data_charts";
 
     public static final String EXTRAS_ADD_FITNESS_CHART = "add_fitness_chart";
     public static final String EXTRAS_ADD_NUTRIITION_CHART = "add_nutrition_chart";
