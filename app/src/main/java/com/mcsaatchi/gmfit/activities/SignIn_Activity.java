ae88fbdc8917bca785b2d2df00b4aff404ad463d
oday.maleh <oday.m@mcsaatchi.me>
2016-05-26 16:32:28 +0300

    Implements Signing In functionality with the API including ALL edge cases/error codes

--- app/src/main/java/com/mcsaatchi/gmfit/activities/SignIn_Activity.java
+++ app/src/main/java/com/mcsaatchi/gmfit/activities/SignIn_Activity.java
@@ -1,13 +1,20 @@
 package com.mcsaatchi.gmfit.activities;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.graphics.Typeface;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.text.SpannableString;
 import android.text.Spanned;
 import android.text.method.LinkMovementMethod;
 import android.text.style.ClickableSpan;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -15,16 +22,28 @@ import android.widget.TextView;
 import com.andreabaccega.widget.FormEditText;
 import com.github.florent37.materialtextfield.MaterialTextField;
 import com.mcsaatchi.gmfit.R;
+import com.mcsaatchi.gmfit.classes.ApiHelper;
+import com.mcsaatchi.gmfit.classes.Cons;
+import com.mcsaatchi.gmfit.classes.EventBus_Poster;
+import com.mcsaatchi.gmfit.classes.EventBus_Singleton;
 import com.mcsaatchi.gmfit.classes.Helpers;
-import com.mcsaatchi.gmfit.logger.Log;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.ArrayList;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class SignIn_Activity extends Base_Activity {
 
+    private static OkHttpClient client = new OkHttpClient();
     @Bind(R.id.emailET)
     FormEditText emailET;
     @Bind(R.id.emailETFloating)
@@ -35,8 +54,8 @@ public class SignIn_Activity extends Base_Activity {
     Button signInBTN;
     @Bind(R.id.forgotPasswordTV)
     TextView forgotPasswordTV;
-
     private ArrayList<FormEditText> allFields = new ArrayList<>();
+    private SharedPreferences prefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +64,8 @@ public class SignIn_Activity extends Base_Activity {
         setContentView(R.layout.activity_sign_in);
 
         ButterKnife.bind(this);
+
+        prefs = getSharedPreferences(Cons.SHARED_PREFS_TITLE, Context.MODE_PRIVATE);
 
         passwordET.setTypeface(Typeface.DEFAULT);
 
@@ -57,9 +78,97 @@ public class SignIn_Activity extends Base_Activity {
             @Override
             public void onClick(View v) {
                 if (Helpers.validateFields(allFields)) {
-                    Log.toaster(SignIn_Activity.this, "All fields check out!");
-                    Intent intent = new Intent(SignIn_Activity.this, GetStarted_Activity.class);
-                    startActivity(intent);
+                    if (Helpers.isInternetAvailable(SignIn_Activity.this)) {
+                        try {
+
+                            final JSONObject jsonForRequest = new JSONObject();
+                            jsonForRequest.put(Cons.REQUEST_PARAM_EMAIL, emailET.getText().toString());
+                            jsonForRequest.put(Cons.REQUEST_PARAM_PASSWORD, passwordET.getText().toString());
+
+                            new AsyncTask<String, String, String>() {
+                                ProgressDialog loggingInDialog;
+
+                                protected void onPreExecute() {
+                                    loggingInDialog = new ProgressDialog(SignIn_Activity.this);
+                                    loggingInDialog.setTitle(getString(R.string.logging_in_dialog_title));
+                                    loggingInDialog.setMessage(getString(R.string.logging_in_dialog_message));
+                                    loggingInDialog.show();
+                                }
+
+                                protected String doInBackground(String... aParams) {
+                                    String userAccessToken = prefs.getString(Cons.PREF_USER_ACCESS_TOKEN, Cons.NO_ACCESS_TOKEN_FOUND_IN_PREFS);
+
+                                    Log.d("ACCESS", "doInBackground: ACCESS TOKEN " + userAccessToken);
+
+                                    RequestBody body = RequestBody.create(Cons.JSON_FORMAT_IDENTIFIER, jsonForRequest.toString());
+                                    Request request = new Request.Builder()
+                                            .url(Cons.ROOT_URL_ADDRESS + Cons.API_NAME_LOGIN)
+                                            .addHeader(Cons.USER_ACCESS_TOKEN_HEADER_PARAMETER, userAccessToken)
+                                            .post(body)
+                                            .build();
+
+                                    try {
+                                        Response response = client.newCall(request).execute();
+                                        return response.body().string();
+                                    } catch (IOException e) {
+                                        e.printStackTrace();
+                                    }
+
+                                    return null;
+                                }
+
+                                protected void onPostExecute(String aResult) {
+
+                                    int responseCode = ApiHelper.parseAPIResponseForCode(aResult);
+
+                                    AlertDialog alertDialog = new AlertDialog.Builder(SignIn_Activity.this).create();
+                                    alertDialog.setTitle(R.string.login_failed_alert_dialog_title);
+                                    alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, getString(R.string.ok),
+                                            new DialogInterface.OnClickListener() {
+                                                public void onClick(DialogInterface dialog, int which) {
+                                                    dialog.dismiss();
+                                                }
+                                            });
+
+                                    loggingInDialog.dismiss();
+
+                                    switch (responseCode) {
+                                        case Cons.REGISTERATION_API_RESPONSE_NOT_PARSED_CORRECTLY:
+                                            alertDialog.setMessage(getString(R.string.error_response_from_server_incorrect));
+                                            alertDialog.show();
+                                            break;
+                                        case Cons.LOGIN_API_WRONG_CREDENTIALS:
+                                            alertDialog.setMessage(getString(R.string.login_failed_wrong_credentials));
+                                            alertDialog.show();
+
+                                            emailET.requestFocus();
+
+                                            break;
+                                        case Cons.REGISTERATION_API_REQUEST_SUCCEEDED_CODE:
+                                            EventBus_Singleton.getInstance().post(new EventBus_Poster(Cons.EVENT_SIGNNED_UP_SUCCESSFULLY_CLOSE_LOGIN_ACTIVITY));
+
+                                            Intent intent = new Intent(SignIn_Activity.this, Main_Activity.class);
+                                            startActivity(intent);
+                                            finish();
+                                            break;
+                                    }
+                                }
+                            }.execute();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    } else {
+                        AlertDialog alertDialog = new AlertDialog.Builder(SignIn_Activity.this).create();
+                        alertDialog.setTitle(R.string.login_failed_alert_dialog_title);
+                        alertDialog.setMessage(getString(R.string.no_internet_connection));
+                        alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, getString(R.string.ok),
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        dialog.dismiss();
+                                    }
+                                });
+                        alertDialog.show();
+                    }
                 }
             }
         });
--- app/src/main/java/com/mcsaatchi/gmfit/activities/SignUp_Activity.java
+++ app/src/main/java/com/mcsaatchi/gmfit/activities/SignUp_Activity.java
@@ -1,13 +1,18 @@
 package com.mcsaatchi.gmfit.activities;
 
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.Typeface;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.text.SpannableString;
 import android.text.Spanned;
 import android.text.method.LinkMovementMethod;
 import android.text.style.ClickableSpan;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -15,29 +20,38 @@ import android.widget.TextView;
 import com.andreabaccega.widget.FormEditText;
 import com.github.florent37.materialtextfield.MaterialTextField;
 import com.mcsaatchi.gmfit.R;
+import com.mcsaatchi.gmfit.classes.ApiHelper;
+import com.mcsaatchi.gmfit.classes.Cons;
+import com.mcsaatchi.gmfit.classes.EventBus_Poster;
+import com.mcsaatchi.gmfit.classes.EventBus_Singleton;
 import com.mcsaatchi.gmfit.classes.Helpers;
-import com.mcsaatchi.gmfit.logger.Log;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.ArrayList;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class SignUp_Activity extends Base_Activity {
 
+    private static OkHttpClient client = new OkHttpClient();
     @Bind(R.id.emailET)
     FormEditText emailET;
     @Bind(R.id.emailETFloating)
     MaterialTextField emailETFloating;
     @Bind(R.id.passwordET)
     FormEditText passwordET;
-    @Bind(R.id.confirmPasswordET)
-    FormEditText confirmPasswordET;
     @Bind(R.id.createAccountBTN)
     Button createAccountBTN;
     @Bind(R.id.creatingAccountTOSTV)
     TextView creatingAccountTOSTV;
-
     private ArrayList<FormEditText> allFields = new ArrayList<>();
 
     @Override
@@ -50,18 +64,109 @@ public class SignUp_Activity extends Base_Activity {
 
         allFields.add(emailET);
         allFields.add(passwordET);
-        allFields.add(confirmPasswordET);
 
         emailETFloating.expand();
 
         passwordET.setTypeface(Typeface.DEFAULT);
-        confirmPasswordET.setTypeface(Typeface.DEFAULT);
 
         createAccountBTN.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (Helpers.validateFields(allFields)){
-                    Log.toaster(SignUp_Activity.this, "All fields check out!");
+                if (Helpers.validateFields(allFields)) {
+                    if (Helpers.isInternetAvailable(SignUp_Activity.this)) {
+                        try {
+
+                            final JSONObject jsonForRequest = new JSONObject();
+                            jsonForRequest.put(Cons.REQUEST_PARAM_EMAIL, emailET.getText().toString());
+                            jsonForRequest.put(Cons.REQUEST_PARAM_PASSWORD, passwordET.getText().toString());
+
+                            new AsyncTask<String, String, String>() {
+                                ProgressDialog signingUpDialog;
+
+                                protected void onPreExecute() {
+                                    signingUpDialog = new ProgressDialog(SignUp_Activity.this);
+                                    signingUpDialog.setTitle(getString(R.string.signing_up_dialog_title));
+                                    signingUpDialog.setMessage(getString(R.string.signing_up_dialog_message));
+                                    signingUpDialog.show();
+                                }
+
+                                protected String doInBackground(String... aParams) {
+                                    RequestBody body = RequestBody.create(Cons.JSON_FORMAT_IDENTIFIER, jsonForRequest.toString());
+                                    Request request = new Request.Builder()
+                                            .url(Cons.ROOT_URL_ADDRESS + Cons.API_NAME_REGISTER)
+                                            .post(body)
+                                            .build();
+
+                                    try {
+                                        Response response = client.newCall(request).execute();
+                                        return response.body().string();
+                                    } catch (IOException e) {
+                                        e.printStackTrace();
+                                    }
+
+                                    return null;
+                                }
+
+                                protected void onPostExecute(String aResult) {
+                                    Log.d("ASYNCRESULT", "onPostExecute: Response was : \n" + aResult);
+
+                                    int responseCode = ApiHelper.parseAPIResponseForCode(aResult);
+
+                                    AlertDialog alertDialog = new AlertDialog.Builder(SignUp_Activity.this).create();
+                                    alertDialog.setTitle(R.string.signing_up_dialog_title);
+                                    alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, getString(R.string.ok),
+                                            new DialogInterface.OnClickListener() {
+                                                public void onClick(DialogInterface dialog, int which) {
+                                                    dialog.dismiss();
+                                                }
+                                            });
+
+                                    signingUpDialog.dismiss();
+
+                                    switch (responseCode) {
+                                        case Cons.REGISTERATION_API_RESPONSE_NOT_PARSED_CORRECTLY:
+                                            alertDialog.setMessage(getString(R.string.error_response_from_server_incorrect));
+                                            alertDialog.show();
+                                            break;
+                                        case Cons.REGISTERATION_API_EMAIL_TAKEN_CODE:
+                                            alertDialog.setMessage(getString(R.string.email_already_taken_api_response));
+                                            alertDialog.show();
+                                            break;
+                                        case Cons.REGISTERATION_API_REQUEST_SUCCEEDED_CODE:
+                                            int operationStatusCode = ApiHelper.parseAndSaveRegisterationToken(SignUp_Activity.this, aResult);
+
+                                            switch (operationStatusCode) {
+                                                case Cons.REGISTRATION_PROCESS_SUCCEEDED_TOKEN_SAVED:
+                                                    EventBus_Singleton.getInstance().post(new EventBus_Poster(Cons.EVENT_SIGNNED_UP_SUCCESSFULLY_CLOSE_LOGIN_ACTIVITY));
+
+                                                    Intent intent = new Intent(SignUp_Activity.this, GetStarted_Activity.class);
+                                                    startActivity(intent);
+                                                    finish();
+                                                    break;
+                                                case Cons.REGISTERATION_API_RESPONSE_NOT_PARSED_CORRECTLY:
+                                                    alertDialog.setMessage(getString(R.string.error_response_from_server_incorrect));
+                                                    alertDialog.show();
+                                                    break;
+                                            }
+                                            break;
+                                    }
+                                }
+                            }.execute();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    } else {
+                        AlertDialog alertDialog = new AlertDialog.Builder(SignUp_Activity.this).create();
+                        alertDialog.setTitle(R.string.signup_failed_alert_dialog_title);
+                        alertDialog.setMessage(getString(R.string.no_internet_connection));
+                        alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, getString(R.string.ok),
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        dialog.dismiss();
+                                    }
+                                });
+                        alertDialog.show();
+                    }
                 }
             }
         });
