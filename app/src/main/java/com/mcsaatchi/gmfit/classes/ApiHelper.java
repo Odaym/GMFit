57e28c1598c63465f6dd2bf695e3fc87fa9fdaca
oday.maleh <oday.m@mcsaatchi.me>
2016-05-26 16:32:52 +0300

    Nice API helper that helps with API'ing

---
+++ app/src/main/java/com/mcsaatchi/gmfit/classes/ApiHelper.java
@@ -0,0 +1,42 @@
+package com.mcsaatchi.gmfit.classes;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class ApiHelper {
+    public static int parseAPIResponseForCode(String responseObject){
+        try {
+            JSONObject jsonResponse = new JSONObject(responseObject);
+            JSONObject dataJSONObject = jsonResponse.getJSONObject("data");
+
+            return dataJSONObject.getInt("code");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return Cons.REGISTERATION_API_RESPONSE_NOT_PARSED_CORRECTLY;
+    }
+
+    public static int parseAndSaveRegisterationToken(Context context, String responseObject){
+        try {
+            JSONObject jsonResponse = new JSONObject(responseObject);
+            JSONObject dataJSONObject = jsonResponse.getJSONObject("data");
+            JSONObject bodyJSONObject = dataJSONObject.getJSONObject("body");
+
+            String userAccessToken = bodyJSONObject.getString("token");
+
+            SharedPreferences prefs = context.getSharedPreferences(Cons.SHARED_PREFS_TITLE, Context.MODE_PRIVATE);
+
+            prefs.edit().putString(Cons.PREF_USER_ACCESS_TOKEN, "Bearer " + userAccessToken).apply();
+
+            return Cons.REGISTRATION_PROCESS_SUCCEEDED_TOKEN_SAVED;
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return Cons.REGISTERATION_API_RESPONSE_NOT_PARSED_CORRECTLY;
+    }
+}
