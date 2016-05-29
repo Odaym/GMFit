c6fab6c535d6e5bc70c78c7d99f727bd350ea6a3
Oday Maleh <oday.maleh@gmail.com>
2016-05-29 20:53:40 +0300

    Grab the user's profile from the API, create a directory for the app, give the file a random name and save it under that directory

--- app/src/main/java/com/mcsaatchi/gmfit/fragments/MainProfile_Fragment.java
+++ app/src/main/java/com/mcsaatchi/gmfit/fragments/MainProfile_Fragment.java
@@ -1,7 +1,7 @@
 package com.mcsaatchi.gmfit.fragments;
 
-import android.Manifest;
 import android.app.ProgressDialog;
+import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -9,12 +9,9 @@ import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.AsyncTask;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -34,6 +31,9 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -49,6 +49,8 @@ public class MainProfile_Fragment extends Fragment {
     @Bind(R.id.emergencyProfileBTN)
     Button emergencyProfileBTN;
     private SharedPreferences prefs;
+
+    private InputStream usableInputStream;
 
     @Override
     public View onCreateView(LayoutInflater inflater,
@@ -112,7 +114,7 @@ public class MainProfile_Fragment extends Fragment {
                                 grabbingUserPolicyDialog.dismiss();
 
                                 switch (responseCode) {
-                                    case Cons.REGISTERATION_API_RESPONSE_NOT_PARSED_CORRECTLY:
+                                    case Cons.API_RESPONSE_NOT_PARSED_CORRECTLY:
 
                                         alertDialog.setMessage(getString(R.string.error_response_from_server_incorrect));
                                         alertDialog.show();
@@ -179,79 +181,11 @@ public class MainProfile_Fragment extends Fragment {
                                 Helpers.showNoInternetDialog(getActivity());
                             } else {
 
-//                                int responseCode = ApiHelper.parseAPIResponseForCode(aResult);
-
-                                final InputStream inputStreamResult = aResult;
-
-                                AlertDialog alertDialog = new AlertDialog.Builder(getActivity()).create();
-                                alertDialog.setTitle(R.string.downloading_pdf_profile_dialog_title);
-                                alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, getString(R.string.ok),
-                                        new DialogInterface.OnClickListener() {
-                                            public void onClick(DialogInterface dialog, int which) {
-                                                dialog.dismiss();
-                                            }
-                                        });
+                                usableInputStream = aResult;
 
                                 downloadingPDFProfileDialog.dismiss();
 
-                                boolean hasPermission = (ContextCompat.checkSelfPermission(getActivity(),
-                                        Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED);
-                                if (!hasPermission) {
-                                    ActivityCompat.requestPermissions(getActivity(),
-                                            new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
-                                            REQUEST_WRITE_STORAGE);
-                                } else {
-                                    new Thread(new Runnable() {
-                                        @Override
-                                        public void run() {
-                                            byte[] buffer = new byte[8 * 1024];
-                                            OutputStream output = null;
-
-                                            try {
-                                                output = new FileOutputStream(Environment.getExternalStorageDirectory() + File.separator + "samplePDF.pdf");
-
-                                                int bytesRead;
-                                                while ((bytesRead = inputStreamResult.read(buffer)) > 0) {
-                                                    output.write(buffer, 0, bytesRead);
-                                                }
-                                            } catch (IOException e) {
-                                                e.printStackTrace();
-                                            } finally {
-                                                try {
-                                                    if (output != null)
-                                                        output.close();
-                                                    inputStreamResult.close();
-
-//                                                    File sdCard = Environment.getExternalStorageDirectory();
-//                                                    File file = new File(sdCard.getAbsolutePath()
-//                                                            + "/Roadie Tuner/Logs/Roadie_Logs_DEBUG.txt");
-                                                    Uri uri = Uri.fromFile(new File(Environment.getExternalStorageDirectory() + File.separator + "samplePDF.pdf"));
-
-
-                                                    Intent i = new Intent(Intent.ACTION_SEND);
-                                                    i.setType("message/rfc822");
-                                                    i.putExtra(Intent.EXTRA_EMAIL,
-                                                            new String[]{"support@gmfit.com"});
-                                                    i.putExtra(Intent.EXTRA_STREAM, uri);
-                                                    i.putExtra(Intent.EXTRA_SUBJECT, "Issue with: "
-                                                            + Build.MANUFACTURER + " " + Build.MODEL + " (" + Build.DEVICE + ") - "
-                                                            + Build.VERSION.RELEASE);
-                                                    i.putExtra(Intent.EXTRA_TEXT, "Testing");
-                                                    try {
-                                                        startActivity(Intent.createChooser(i, "send email through"));
-                                                    } catch (android.content.ActivityNotFoundException ex) {
-                                                        Toast.makeText(getActivity(),
-                                                                "No email client", Toast.LENGTH_SHORT)
-                                                                .show();
-                                                    }
-                                                } catch (IOException e) {
-                                                    e.printStackTrace();
-                                                }
-                                            }
-
-                                        }
-                                    }).start();
-                                }
+                                getUserEmergencyProfile(usableInputStream);
                             }
                         }
                     }.execute();
@@ -271,11 +205,80 @@ public class MainProfile_Fragment extends Fragment {
             case REQUEST_WRITE_STORAGE: {
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     //reload my activity with permission granted or use the features what required the permission
+                    getUserEmergencyProfile(usableInputStream);
                 } else {
                     Toast.makeText(getActivity(), "The app was not allowed to write to your storage. Hence, it cannot function properly. Please consider granting it " +
                             "this permission", Toast.LENGTH_LONG).show();
                 }
             }
         }
+    }
+
+    private void getUserEmergencyProfile(final InputStream inputStreamResult) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                byte[] buffer = new byte[8 * 1024];
+
+                OutputStream output = null;
+
+                File folder = new File(Environment.getExternalStorageDirectory() + "/GMFit/");
+
+                boolean createDirectorySuccess = true;
+
+                if (!folder.exists()) {
+                    createDirectorySuccess = folder.mkdir();
+                }
+
+                if (createDirectorySuccess) {
+                    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("ddMMyy-hhmmss_SSS", Locale.getDefault());
+                    String profileFileName = folder.toString() + File.separator + simpleDateFormat.format(new Date()) + ".pdf";
+
+                    Log.d("PROFILE", "run: Profile filename " + profileFileName);
+                    try {
+                        output = new FileOutputStream(profileFileName);
+
+                        int bytesRead;
+                        while ((bytesRead = inputStreamResult.read(buffer)) > 0) {
+                            output.write(buffer, 0, bytesRead);
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } finally {
+                        try {
+                            if (output != null)
+                                output.close();
+                            inputStreamResult.close();
+
+                            Uri uri = Uri.fromFile(new File(profileFileName));
+
+
+                            Intent i = new Intent(Intent.ACTION_SEND);
+                            i.setType("message/rfc822");
+                            i.putExtra(Intent.EXTRA_EMAIL,
+                                    new String[]{"support@gmfit.com"});
+                            i.putExtra(Intent.EXTRA_STREAM, uri);
+                            i.putExtra(Intent.EXTRA_SUBJECT, "EMERGENCY PROFILE");
+//                                + Build.MANUFACTURER + " " + Build.MODEL + " (" + Build.DEVICE + ") - "
+//                                + Build.VERSION.RELEASE);
+                            i.putExtra(Intent.EXTRA_TEXT, "This is an emergency, please check my profile.");
+                            try {
+                                startActivity(Intent.createChooser(i, "Send email through"));
+                            } catch (ActivityNotFoundException ex) {
+                                Toast.makeText(getActivity(),
+                                        "No email client", Toast.LENGTH_SHORT)
+                                        .show();
+                            }
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                } else {
+                    //TODO:
+                    //Directory creation failed
+                }
+            }
+        }).start();
     }
 }
