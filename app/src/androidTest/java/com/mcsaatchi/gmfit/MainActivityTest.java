49ed02a0eaf2506c5a1251a28e6d325bcca0df1f
oday.maleh <oday.m@mcsaatchi.me>
2016-05-31 14:19:33 +0300

    Editing some tests, namely the titles and annotation of each

--- app/src/androidTest/java/com/mcsaatchi/gmfit/MainActivityTest.java
+++ app/src/androidTest/java/com/mcsaatchi/gmfit/MainActivityTest.java
@@ -1,14 +1,23 @@
 package com.mcsaatchi.gmfit;
 
 import android.support.design.widget.CoordinatorLayout;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
 import android.support.v4.widget.NestedScrollView;
 import android.test.ActivityInstrumentationTestCase2;
 import android.widget.FrameLayout;
 
 import com.mcsaatchi.gmfit.activities.Main_Activity;
 
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
 public class MainActivityTest extends ActivityInstrumentationTestCase2<Main_Activity> {
 
+    @Rule
+    public ActivityTestRule<Main_Activity> mainActivityActivityTestRule = new ActivityTestRule<Main_Activity>(Main_Activity.class);
     private Main_Activity mainActivity;
 
     public MainActivityTest() {
@@ -22,19 +31,24 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<Main_Acti
         mainActivity = getActivity();
     }
 
-    public void testCoordinatorLayoutNotNull(){
+    @Test
+    public void clickAddChartButton_opensAddChartUi() throws Exception{
+    }
+
+    @Test
+    public void coordinatorLayoutNotNull() {
         CoordinatorLayout coordinatorLayout = (CoordinatorLayout) mainActivity.findViewById(R.id.myCoordinator);
 
         assertNotNull(coordinatorLayout);
     }
 
-    public void testNestedScrollViewNotNull(){
+    public void nestedScrollViewNotNull() {
         NestedScrollView nestedScrollView = (NestedScrollView) mainActivity.findViewById(R.id.myScrollingContent);
 
         assertNotNull(nestedScrollView);
     }
 
-    public void testFragmentContainerNotNull(){
+    public void fragmentContainerNotNull() {
         FrameLayout fragmentContainer = (FrameLayout) mainActivity.findViewById(R.id.fragment_container);
 
         assertNotNull(fragmentContainer);
