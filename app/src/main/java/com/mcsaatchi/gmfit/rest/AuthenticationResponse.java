5768a4314bfeef96878a08a1e9562eb8b9dfd761
oday.maleh <oday.m@mcsaatchi.me>
2016-06-14 15:46:48 +0300

    POJO models of how the API response for authentication will be returned. This will be used by RetroFit's GSON to marshall the JSON response into actual objects that can be grabbed later from code, they need to match exactly

---
+++ app/src/main/java/com/mcsaatchi/gmfit/rest/AuthenticationResponse.java
@@ -0,0 +1,25 @@
+package com.mcsaatchi.gmfit.rest;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class AuthenticationResponse {
+
+    @SerializedName("data")
+    @Expose
+    private AuthenticationResponseData data;
+
+    /**
+     * @return The data
+     */
+    public AuthenticationResponseData getData() {
+        return data;
+    }
+
+    /**
+     * @param data The data
+     */
+    public void setData(AuthenticationResponseData data) {
+        this.data = data;
+    }
+}
