93ed1d67b41c62a9df6f4223fceb5b709a54e131
Oday Maleh <oday.maleh@gmail.com>
2016-06-14 20:40:29 +0300

    These classes are the POJO representation of the JSON response for a Get User Policy request

---
+++ app/src/main/java/com/mcsaatchi/gmfit/rest/UserPolicyResponseBody.java
@@ -0,0 +1,28 @@
+package com.mcsaatchi.gmfit.rest;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class UserPolicyResponseBody {
+    @SerializedName("user_policy")
+    @Expose
+    private String userPolicy;
+
+    /**
+     *
+     * @return
+     * The userPolicy
+     */
+    public String getUserPolicy() {
+        return userPolicy;
+    }
+
+    /**
+     *
+     * @param userPolicy
+     * The user_policy
+     */
+    public void setUserPolicy(String userPolicy) {
+        this.userPolicy = userPolicy;
+    }
+}
