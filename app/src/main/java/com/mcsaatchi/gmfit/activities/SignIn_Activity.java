869a09796873f8f92226b55e70f2002d69927227
oday.maleh <oday.m@mcsaatchi.me>
2016-06-14 15:40:14 +0300

    Finalize API call with RetroFit with error handling "Sign In"

--- app/src/main/java/com/mcsaatchi/gmfit/activities/SignIn_Activity.java
+++ app/src/main/java/com/mcsaatchi/gmfit/activities/SignIn_Activity.java
@@ -1,9 +1,14 @@
 package com.mcsaatchi.gmfit.activities;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.text.SpannableString;
 import android.text.Spanned;
 import android.text.method.LinkMovementMethod;
@@ -14,9 +19,12 @@ import android.widget.TextView;
 
 import com.andreabaccega.widget.FormEditText;
 import com.mcsaatchi.gmfit.R;
-import com.mcsaatchi.gmfit.classes.ApiHelper;
 import com.mcsaatchi.gmfit.classes.Cons;
+import com.mcsaatchi.gmfit.classes.EventBus_Poster;
+import com.mcsaatchi.gmfit.classes.EventBus_Singleton;
 import com.mcsaatchi.gmfit.classes.Helpers;
+import com.mcsaatchi.gmfit.rest.AuthenticationResponse;
+import com.mcsaatchi.gmfit.rest.RestClient;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -25,6 +33,9 @@ import java.util.ArrayList;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class SignIn_Activity extends Base_Activity {
 
@@ -38,6 +49,8 @@ public class SignIn_Activity extends Base_Activity {
     TextView forgotPasswordTV;
     private ArrayList<FormEditText> allFields = new ArrayList<>();
 
+    private SharedPreferences prefs;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(Helpers.createActivityBundleWithProperties(R.string.sign_in_activity_title, true));
@@ -45,6 +58,8 @@ public class SignIn_Activity extends Base_Activity {
         setContentView(R.layout.activity_sign_in);
 
         ButterKnife.bind(this);
+
+        prefs = getSharedPreferences(Cons.SHARED_PREFS_TITLE, Context.MODE_PRIVATE);
 
         passwordET.setTypeface(Typeface.DEFAULT);
 
@@ -60,8 +75,10 @@ public class SignIn_Activity extends Base_Activity {
                         jsonForRequest.put(Cons.REQUEST_PARAM_EMAIL, emailET.getText().toString());
                         jsonForRequest.put(Cons.REQUEST_PARAM_PASSWORD, passwordET.getText().toString());
 
-                        ApiHelper.runApiAsyncTask(SignIn_Activity.this, Cons.API_NAME_SIGN_IN, Cons.POST_REQUEST_TYPE, jsonForRequest, R.string
-                                .logging_in_dialog_title, R.string.logging_in_dialog_message, null);
+                        if (Helpers.isInternetAvailable(SignIn_Activity.this))
+                            signInUser(emailET.getText().toString(), passwordET.getText().toString());
+                        else
+                            Helpers.showNoInternetDialog(SignIn_Activity.this);
 
                     } catch (JSONException e) {
                         e.printStackTrace();
@@ -82,5 +99,69 @@ public class SignIn_Activity extends Base_Activity {
         forgotPasswordTV.setText(ss);
         forgotPasswordTV.setMovementMethod(LinkMovementMethod.getInstance());
         forgotPasswordTV.setHighlightColor(Color.BLUE);
+    }
+
+    private void signInUser(String email, String password) {
+        final ProgressDialog waitingDialog = new ProgressDialog(this);
+        waitingDialog.setTitle(getString(R.string.signing_in_dialog_title));
+        waitingDialog.setMessage(getString(R.string.signing_in_dialog_message));
+        waitingDialog.show();
+
+        final AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+        alertDialog.setTitle(R.string.signing_in_dialog_title);
+        alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, getString(R.string.ok),
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+
+                        if (waitingDialog.isShowing())
+                            waitingDialog.dismiss();
+                    }
+                });
+
+        Call<AuthenticationResponse> registerUserCall = new RestClient().getGMFitService().signInUser(new SignInRequest(email,
+                password));
+
+        registerUserCall.enqueue(new Callback<AuthenticationResponse>() {
+            @Override
+            public void onResponse(Call<AuthenticationResponse> call, Response<AuthenticationResponse> response) {
+                if (response.body() != null) {
+                    switch (response.code()) {
+                        case Cons.API_REQUEST_SUCCEEDED_CODE:
+                            waitingDialog.dismiss();
+
+                            prefs.edit().putString(Cons.PREF_USER_ACCESS_TOKEN, "Bearer " + response.body().getData().getBody().getToken()).apply();
+
+                            EventBus_Singleton.getInstance().post(new EventBus_Poster(Cons.EVENT_SIGNNED_UP_SUCCESSFULLY_CLOSE_LOGIN_ACTIVITY));
+
+                            Intent intent = new Intent(SignIn_Activity.this, Main_Activity.class);
+                            startActivity(intent);
+                            finish();
+
+                            break;
+                        case Cons.LOGIN_API_WRONG_CREDENTIALS:
+                            alertDialog.setMessage(getString(R.string.login_failed_wrong_credentials));
+                            alertDialog.show();
+                            break;
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<AuthenticationResponse> call, Throwable t) {
+                alertDialog.setMessage(getString(R.string.error_response_from_server_incorrect));
+                alertDialog.show();
+            }
+        });
+    }
+
+    public class SignInRequest {
+        final String email;
+        final String password;
+
+        public SignInRequest(String email, String password) {
+            this.email = email;
+            this.password = password;
+        }
     }
 }
