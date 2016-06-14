939fbc3256cdfa2b2c7c9a2b26db3df20127a389
oday.maleh <oday.m@mcsaatchi.me>
2016-06-14 17:13:37 +0300

    Implementing the updateUserProfile interface method

--- app/src/main/java/com/mcsaatchi/gmfit/rest/GMFit_Service.java
+++ app/src/main/java/com/mcsaatchi/gmfit/rest/GMFit_Service.java
@@ -1,24 +1,28 @@
 package com.mcsaatchi.gmfit.rest;
 
+import com.mcsaatchi.gmfit.activities.SignIn_Activity;
 import com.mcsaatchi.gmfit.activities.SignUp_Activity;
 import com.mcsaatchi.gmfit.classes.Cons;
+import com.mcsaatchi.gmfit.fragments.Setup_Profile_3_Fragment;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.POST;
-import retrofit2.http.Query;
-import rx.Observable;
 
 public interface GMFit_Service {
 
-    @GET("logout")
-    Observable<DefaultGetResponse> signOutUser(@Header(Cons.USER_ACCESS_TOKEN_HEADER_PARAMETER) String userAccessToken);
+    @POST("login")
+    Call<AuthenticationResponse> signInUser(@Body SignIn_Activity.SignInRequest userCredentails);
 
     @POST("register")
-    Observable<RegisterResponse> registerUser(@Body SignUp_Activity.RegisterRequest userCredentials);
+    Call<AuthenticationResponse> registerUser(@Body SignUp_Activity.RegisterRequest userCredentials);
 
-    @GET("user/metrics")
-    Call<MetricsResponse> getChartMetricsByDate(@Header(Cons.USER_ACCESS_TOKEN_HEADER_PARAMETER) String userAccessToken, @Query("start_date") String start_date, @Query("end_date") String end_date, @Query("type") String type, @Query("monitored_metrics") String monitored_metrics);
+    @GET("logout")
+    Call<DefaultGetResponse> signOutUser(@Header(Cons.USER_ACCESS_TOKEN_HEADER_PARAMETER) String userAccessToken);
+
+    @POST("user/update-profile")
+    Call<AuthenticationResponse> updateUserProfile(@Header(Cons.USER_ACCESS_TOKEN_HEADER_PARAMETER) String userAccessToken, @Body Setup_Profile_3_Fragment
+            .UpdateProfileRequest updateProfileRequest);
 }
