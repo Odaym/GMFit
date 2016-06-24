9ea1015548e6077705b89445cdc9107b771ab360
Oday Maleh <oday.maleh@gmail.com>
2016-06-24 23:52:39 +0300

    The gridview adapter is now universal, accepts a map of values and a way of laying out those values, works for Health, Nutrition and Fitness

---
+++ app/src/main/java/com/mcsaatchi/gmfit/adapters/Widgets_GridAdapter.java
@@ -0,0 +1,99 @@
+package com.mcsaatchi.gmfit.adapters;
+
+import android.content.Context;
+import android.support.design.internal.ParcelableSparseArray;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.mcsaatchi.gmfit.R;
+import com.mcsaatchi.gmfit.classes.ParcelableFitnessString;
+import com.mcsaatchi.gmfit.classes.ParcelableNutritionString;
+
+import java.text.NumberFormat;
+
+public class Widgets_GridAdapter extends BaseAdapter {
+    private Context context;
+    private ParcelableSparseArray widgetsMap;
+    private int layoutResourceId;
+
+    public Widgets_GridAdapter(Context context, ParcelableSparseArray widgetsMap, int layoutResourceId) {
+        this.context = context;
+        this.widgetsMap = widgetsMap;
+        this.layoutResourceId = layoutResourceId;
+    }
+
+    public int getCount() {
+        return widgetsMap.size();
+    }
+
+    public ParcelableFitnessString getItem(int position) {
+        return (ParcelableFitnessString) widgetsMap.valueAt(position);
+    }
+
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    // create a new ImageView for each item referenced by the Adapter
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+
+        if (convertView == null) {
+            LayoutInflater inflater = (LayoutInflater) context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(layoutResourceId, parent,
+                    false);
+
+            holder = new ViewHolder();
+
+            switch (layoutResourceId) {
+                case R.layout.grid_item_fitness_widgets:
+                    holder.metricTV = (TextView) convertView.findViewById(R.id.metricTV);
+                    holder.metricIcon = (ImageView) convertView.findViewById(R.id.metricIMG);
+                    break;
+                case R.layout.grid_item_nutrition_widgets:
+                    holder.metricTV = (TextView) convertView.findViewById(R.id.metricTV);
+                    holder.metricPercentageTV = (TextView) convertView.findViewById(R.id.metricPercentageTV);
+                    holder.measurementUnitTV = (TextView) convertView.findViewById(R.id.measurementUnitTV);
+                    holder.metricTitleTV = (TextView) convertView.findViewById(R.id.metricTitleTV);
+                    break;
+            }
+
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        switch(layoutResourceId){
+            case R.layout.grid_item_fitness_widgets:
+                holder.metricTV.setText(NumberFormat.getInstance().format(Double.parseDouble(String.valueOf(((ParcelableFitnessString) widgetsMap.valueAt(position))
+                        .getValue()))));
+
+                holder.metricIcon.setImageResource(((ParcelableFitnessString) widgetsMap.valueAt(position)).getKey());
+                break;
+            case R.layout.grid_item_nutrition_widgets:
+                holder.metricTV.setText(NumberFormat.getInstance().format(Double.parseDouble(String.valueOf(((ParcelableNutritionString) widgetsMap.valueAt(position))
+                        .getValue()))));
+
+                holder.metricTitleTV.setText(((ParcelableNutritionString) widgetsMap.valueAt(position)).getTitle());
+                holder.metricPercentageTV.setText("(" + ((ParcelableNutritionString) widgetsMap.valueAt(position)).getPercentage() + "%)");
+                holder.measurementUnitTV.setText(((ParcelableNutritionString) widgetsMap.valueAt(position)).getMeasurementUnit());
+
+                break;
+        }
+
+        return convertView;
+    }
+
+    class ViewHolder {
+        ImageView metricIcon;
+        TextView metricTV;
+        TextView measurementUnitTV;
+        TextView metricPercentageTV;
+        TextView metricTitleTV;
+    }
+}
