4fb256c0de8b92225788e35c4466e3b2cba048ae
oday.maleh <oday.m@mcsaatchi.me>
2016-07-11 12:47:50 +0300

    Finalizing UI in all these fragments

--- app/src/main/java/com/mcsaatchi/gmfit/activities/AddNewMedicalCondition_Activity.java
+++ app/src/main/java/com/mcsaatchi/gmfit/activities/AddNewMedicalCondition_Activity.java
@@ -5,11 +5,15 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.Toolbar;
-import android.widget.LinearLayout;
+import android.widget.CompoundButton;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.RelativeLayout;
 
 import com.mcsaatchi.gmfit.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -23,6 +27,8 @@ public class AddNewMedicalCondition_Activity extends Base_Activity {
 
     private String[] medicalConditions = new String[]{"Hypertension", "Diabetes", "Obesity", "Asthma", "Arthritis", "Osteoporosis"};
 
+    private List<RadioButton> radioButtonsList = new ArrayList<>();
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,22 +41,34 @@ public class AddNewMedicalCondition_Activity extends Base_Activity {
         setSupportActionBar(toolbar);
         setupToolbar(toolbar);
 
-        for (int i = 0; i < medicalConditions.length; i++) {
-            RadioButton radioButtonView = (RadioButton) getLayoutInflater().inflate(R.layout.radio_button_list_item_add_medical_condition, null);
+        for (String medicalCondition : medicalConditions) {
+            RelativeLayout radioButtonView = (RelativeLayout) getLayoutInflater().inflate(R.layout.radio_button_list_item_add_medical_condition, null);
 
-            LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
-                    LinearLayout.LayoutParams.MATCH_PARENT, getResources().getDimensionPixelSize(R.dimen.radiobutton_row_height));
+            final RadioButton actualRadioBTN = (RadioButton) radioButtonView.findViewById(R.id.actualRadioBTN);
 
-            radioButtonView.setLayoutParams(lp);
+            actualRadioBTN.setText(medicalCondition);
 
-            radioButtonView.setText(medicalConditions[i]);
+            radioButtonView.setPadding(0, 0, 0, getResources().getDimensionPixelSize(R.dimen.default_margin_0));
 
             medicalConditionsRadioGroup.addView(radioButtonView);
+
+            actualRadioBTN.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton compoundButton, boolean checked) {
+                    for (int i = 0; i < radioButtonsList.size(); i++) {
+                        radioButtonsList.get(i).setChecked(false);
+                    }
+
+                    actualRadioBTN.setChecked(checked);
+                }
+            });
+
+            radioButtonsList.add(actualRadioBTN);
         }
     }
 
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
-    private void setupToolbar(Toolbar toolbar){
+    private void setupToolbar(Toolbar toolbar) {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         toolbar.setNavigationIcon(getDrawable(R.drawable.ic_arrow_left));
         toolbar.setTitleTextAppearance(this, R.style.Toolbar_TitleTextStyle);
