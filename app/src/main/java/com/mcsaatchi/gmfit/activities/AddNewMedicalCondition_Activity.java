0671433a0ac72bc6bacfebac1d031cfcca74da22
oday.maleh <oday.m@mcsaatchi.me>
2016-07-13 11:09:08 +0300

    Items added here in the Add Medical Conditions are now Checkboxes not RadioButtons because they allow multiple choice

--- app/src/main/java/com/mcsaatchi/gmfit/activities/AddNewMedicalCondition_Activity.java
+++ app/src/main/java/com/mcsaatchi/gmfit/activities/AddNewMedicalCondition_Activity.java
@@ -3,15 +3,11 @@ package com.mcsaatchi.gmfit.activities;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.Toolbar;
-import android.widget.CompoundButton;
-import android.widget.RadioButton;
+import android.widget.CheckBox;
 import android.widget.RadioGroup;
 import android.widget.RelativeLayout;
 
 import com.mcsaatchi.gmfit.R;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -25,8 +21,6 @@ public class AddNewMedicalCondition_Activity extends Base_Activity {
 
     private String[] medicalConditions = new String[]{"Hypertension", "Diabetes", "Obesity", "Asthma", "Arthritis", "Osteoporosis"};
 
-    private List<RadioButton> radioButtonsList = new ArrayList<>();
-
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,35 +30,23 @@ public class AddNewMedicalCondition_Activity extends Base_Activity {
         ButterKnife.bind(this);
 
         setupToolbar(toolbar, R.string.add_medical_condition_activity_title, true);
+        addTopPaddingToolbar(toolbar);
 
         for (String medicalCondition : medicalConditions) {
-            RelativeLayout radioButtonView = (RelativeLayout) getLayoutInflater().inflate(R.layout.radio_button_list_item_add_medical_condition, null);
+            RelativeLayout checkBoxView = (RelativeLayout) getLayoutInflater().inflate(R.layout.checkbox_list_item_add_medical_condition, null);
 
 
-            final RadioButton actualRadioBTN = (RadioButton) radioButtonView.findViewById(R.id.actualRadioBTN);
+            final CheckBox actualCheckBox = (CheckBox) checkBoxView.findViewById(R.id.actualCheckBox);
 
-            actualRadioBTN.setText(medicalCondition);
+            actualCheckBox.setText(medicalCondition);
 
-            radioButtonView.setPadding(0, 0, 0, getResources().getDimensionPixelSize(R.dimen.default_margin_0));
+            checkBoxView.setPadding(0, 0, 0, getResources().getDimensionPixelSize(R.dimen.default_margin_0));
 
-            radioButtonView.setLayoutParams(new RelativeLayout.LayoutParams(
+            checkBoxView.setLayoutParams(new RelativeLayout.LayoutParams(
                     RelativeLayout.LayoutParams.WRAP_CONTENT,
                     RelativeLayout.LayoutParams.WRAP_CONTENT));
 
-            medicalConditionsRadioGroup.addView(radioButtonView);
-
-            actualRadioBTN.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-                @Override
-                public void onCheckedChanged(CompoundButton compoundButton, boolean checked) {
-                    for (int i = 0; i < radioButtonsList.size(); i++) {
-                        radioButtonsList.get(i).setChecked(false);
-                    }
-
-                    actualRadioBTN.setChecked(checked);
-                }
-            });
-
-            radioButtonsList.add(actualRadioBTN);
+            medicalConditionsRadioGroup.addView(checkBoxView);
         }
     }
 }
