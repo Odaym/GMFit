50fa76e5e76ac354ce2a876dd61e7f122636b525
oday.maleh <oday.m@mcsaatchi.me>
2016-08-11 16:27:20 +0300

    Sign the user in silently upon opening the app, save the credentials when the login succeeds, if there is no Internet close down the app

--- app/src/main/java/com/mcsaatchi/gmfit/activities/Splash_Activity.java
+++ app/src/main/java/com/mcsaatchi/gmfit/activities/Splash_Activity.java
@@ -6,14 +6,25 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 
 import com.mcsaatchi.gmfit.R;
 import com.mcsaatchi.gmfit.classes.Cons;
+import com.mcsaatchi.gmfit.classes.Helpers;
+import com.mcsaatchi.gmfit.rest.AuthenticationResponse;
+import com.mcsaatchi.gmfit.rest.AuthenticationResponseChart;
+import com.mcsaatchi.gmfit.rest.AuthenticationResponseInnerBody;
+import com.mcsaatchi.gmfit.rest.AuthenticationResponseWidget;
+import com.mcsaatchi.gmfit.rest.RestClient;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class Splash_Activity extends AppCompatActivity {
 
-    private static final String TAG = "Splash_Activity";
     private SharedPreferences prefs;
     private Intent intent;
 
@@ -25,13 +36,23 @@ public class Splash_Activity extends AppCompatActivity {
         prefs = getSharedPreferences(Cons.SHARED_PREFS_TITLE, Context.MODE_PRIVATE);
 
         int SPLASH_TIME_OUT = 1000;
+        int NO_INTERNET_DIALOG_TIMEOUT = 3000;
 
-        if (prefs.getBoolean(Cons.EXTRAS_USER_LOGGED_IN, true)) {
-            Log.d(TAG, "onCreate: User is logged in");
-            intent = new Intent(Splash_Activity.this, Main_Activity.class);
-            startActivity(intent);
+        if (prefs.getBoolean(Cons.EXTRAS_USER_LOGGED_IN, false)) {
+
+            if (Helpers.isInternetAvailable(Splash_Activity.this)) {
+                signInUserSilently(prefs.getString(Cons.EXTRAS_USER_EMAIL, ""), prefs.getString(Cons.EXTRAS_USER_PASSWORD, ""));
+            } else {
+                Helpers.showNoInternetDialog(Splash_Activity.this);
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        finish();
+                    }
+                }, NO_INTERNET_DIALOG_TIMEOUT);
+            }
+
         } else {
-            Log.d(TAG, "onCreate: User is NOT logged in");
             new Handler().postDelayed(new Runnable() {
 
                 @Override
@@ -40,6 +61,52 @@ public class Splash_Activity extends AppCompatActivity {
                     startActivity(intent);
                 }
             }, SPLASH_TIME_OUT);
+        }
+    }
+
+    public void signInUserSilently(String email, String password) {
+        Call<AuthenticationResponse> signInUserCall = new RestClient().getGMFitService().signInUserSilently(new SignInRequest(email,
+                password));
+
+        signInUserCall.enqueue(new Callback<AuthenticationResponse>() {
+            @Override
+            public void onResponse(Call<AuthenticationResponse> call, Response<AuthenticationResponse> response) {
+                switch (response.code()) {
+                    case 200:
+                        AuthenticationResponseInnerBody responseBody = response.body().getData().getBody();
+
+                        //Refreshes access token
+                        prefs.edit().putString(Cons.PREF_USER_ACCESS_TOKEN, "Bearer " + responseBody.getToken()).apply();
+
+                        List<AuthenticationResponseWidget> widgetsMap = responseBody.getWidgets();
+                        List<AuthenticationResponseChart> chartsMap = responseBody.getCharts();
+
+                        Intent intent = new Intent(Splash_Activity.this, Main_Activity.class);
+                        intent.putParcelableArrayListExtra("widgets", (ArrayList<AuthenticationResponseWidget>) widgetsMap);
+                        intent.putParcelableArrayListExtra("charts", (ArrayList<AuthenticationResponseChart>) chartsMap);
+                        startActivity(intent);
+
+                        finish();
+
+                        break;
+                }
+            }
+
+            @Override
+            public void onFailure(Call<AuthenticationResponse> call, Throwable t) {
+//                alertDialog.setMessage(getString(R.string.error_response_from_server_incorrect));
+//                alertDialog.show();
+            }
+        });
+    }
+
+    public class SignInRequest {
+        final String email;
+        final String password;
+
+        public SignInRequest(String email, String password) {
+            this.email = email;
+            this.password = password;
         }
     }
 }
