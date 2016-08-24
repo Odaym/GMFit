8a221234dfa7763313902e53781683c309b7c614
oday.maleh <oday.m@mcsaatchi.me>
2016-08-24 17:08:35 +0300

    Slug Breakdown API response Java model representation (7 classes)

---
+++ app/src/main/java/com/mcsaatchi/gmfit/rest/SlugBreakdownResponse.java
@@ -0,0 +1,23 @@
+package com.mcsaatchi.gmfit.rest;
+
+public class SlugBreakdownResponse {
+    private SlugBreakdownResponseData data;
+
+    /**
+     *
+     * @return
+     * The data
+     */
+    public SlugBreakdownResponseData getData() {
+        return data;
+    }
+
+    /**
+     *
+     * @param data
+     * The data
+     */
+    public void setData(SlugBreakdownResponseData data) {
+        this.data = data;
+    }
+}
---
+++ app/src/main/java/com/mcsaatchi/gmfit/rest/SlugBreakdownResponseInnerData.java
@@ -0,0 +1,105 @@
+package com.mcsaatchi.gmfit.rest;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SlugBreakdownResponseInnerData implements Parcelable {
+    public static final Creator<SlugBreakdownResponseInnerData> CREATOR = new Creator<SlugBreakdownResponseInnerData>() {
+        @Override
+        public SlugBreakdownResponseInnerData createFromParcel(Parcel in) {
+            return new SlugBreakdownResponseInnerData(in);
+        }
+
+        @Override
+        public SlugBreakdownResponseInnerData[] newArray(int size) {
+            return new SlugBreakdownResponseInnerData[size];
+        }
+    };
+    @SerializedName("daily")
+    @Expose
+    private List<SlugBreakdownResponseDaily> daily = new ArrayList<>();
+    @SerializedName("monthly")
+    @Expose
+    private List<SlugBreakdownResponseMonthly> monthly = new ArrayList<>();
+    @SerializedName("yearly")
+    @Expose
+    private List<SlugBreakdownResponseYearly> yearly = new ArrayList<>();
+
+    protected SlugBreakdownResponseInnerData(Parcel in) {
+        daily = in.createTypedArrayList(SlugBreakdownResponseDaily.CREATOR);
+        monthly = in.createTypedArrayList(SlugBreakdownResponseMonthly.CREATOR);
+        yearly = in.createTypedArrayList(SlugBreakdownResponseYearly.CREATOR);
+    }
+
+    /**
+     *
+     * @return
+     * The daily
+     */
+    public List<SlugBreakdownResponseDaily> getDaily() {
+        return daily;
+    }
+
+    /**
+     *
+     * @param daily
+     * The daily
+     */
+    public void setDaily(List<SlugBreakdownResponseDaily> daily) {
+        this.daily = daily;
+    }
+
+    /**
+     *
+     * @return
+     * The monthly
+     */
+    public List<SlugBreakdownResponseMonthly> getMonthly() {
+        return monthly;
+    }
+
+    /**
+     *
+     * @param monthly
+     * The monthly
+     */
+    public void setMonthly(List<SlugBreakdownResponseMonthly> monthly) {
+        this.monthly = monthly;
+    }
+
+    /**
+     *
+     * @return
+     * The yearly
+     */
+    public List<SlugBreakdownResponseYearly> getYearly() {
+        return yearly;
+    }
+
+    /**
+     *
+     * @param yearly
+     * The yearly
+     */
+    public void setYearly(List<SlugBreakdownResponseYearly> yearly) {
+        this.yearly = yearly;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeTypedList(daily);
+        parcel.writeTypedList(monthly);
+        parcel.writeTypedList(yearly);
+    }
+}
