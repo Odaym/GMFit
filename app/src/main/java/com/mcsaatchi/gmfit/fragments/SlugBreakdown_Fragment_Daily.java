acfbaa72894d02a7fe1562ce301c7b50d7a87b7c
oday.maleh <oday.m@mcsaatchi.me>
2016-08-24 17:09:42 +0300

    A Fragment for each of the Slug Breakdown views (Daily, Monthly and Yearly) along with their Fragment layout files AND the layout files corresponding to the list items shown in the lists of each of those 3 views.

---
+++ app/src/main/java/com/mcsaatchi/gmfit/fragments/SlugBreakdown_Fragment_Daily.java
@@ -0,0 +1,114 @@
+package com.mcsaatchi.gmfit.fragments;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.mcsaatchi.gmfit.R;
+import com.mcsaatchi.gmfit.classes.Cons;
+import com.mcsaatchi.gmfit.rest.SlugBreakdownResponseDaily;
+
+import java.util.ArrayList;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class SlugBreakdown_Fragment_Daily extends Fragment {
+    @Bind(R.id.slugBreakdownListView)
+    ListView slugBreakdownListView;
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View fragmentView = inflater.inflate(R.layout.fragment_slug_breakdown_daily, null);
+
+        ButterKnife.bind(this, fragmentView);
+
+        Bundle fragmentBundle = getArguments();
+
+        if (fragmentBundle != null) {
+            ArrayList<Parcelable> slugBreakdownData = fragmentBundle.getParcelableArrayList(Cons.BUNDLE_SLUG_BREAKDOWN_DATA_DAILY);
+
+            hookupListWithItems(slugBreakdownData);
+        }
+
+
+        return fragmentView;
+    }
+
+    private void hookupListWithItems(ArrayList<Parcelable> items) {
+        SlugBreakdown_ListAdapter slugBreakdownListAdapter = new SlugBreakdown_ListAdapter(getActivity(), items);
+        slugBreakdownListView.setAdapter(slugBreakdownListAdapter);
+    }
+
+    public class SlugBreakdown_ListAdapter extends BaseAdapter {
+
+        private ArrayList<Parcelable> slugBreakdownData;
+        private Context context;
+
+        public SlugBreakdown_ListAdapter(Context context, ArrayList<Parcelable> slugBreakdownData) {
+            super();
+            this.context = context;
+            this.slugBreakdownData = slugBreakdownData;
+        }
+
+        @Override
+        public int getCount() {
+            return slugBreakdownData.size();
+        }
+
+        @Override
+        public SlugBreakdownResponseDaily getItem(int index) {
+            return (SlugBreakdownResponseDaily) slugBreakdownData.get(index);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder holder;
+            if (convertView == null) {
+                LayoutInflater inflater = (LayoutInflater) context
+                        .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                convertView = inflater.inflate(R.layout.list_item_slug_daily_breakdown, parent,
+                        false);
+
+                holder = new ViewHolder();
+
+                holder.slugDateTV = (TextView) convertView.findViewById(R.id.slugDateTV);
+                holder.slugTotalTV = (TextView) convertView.findViewById(R.id.slugTotalTV);
+
+                convertView.setTag(holder);
+            } else {
+                holder = (ViewHolder) convertView.getTag();
+            }
+
+            holder.slugDateTV.setText(getItem(position).getDate());
+            holder.slugTotalTV.setText(getItem(position).getTotal());
+
+            return convertView;
+        }
+
+        class ViewHolder {
+            TextView slugDateTV;
+            TextView slugTotalTV;
+        }
+    }
+}
