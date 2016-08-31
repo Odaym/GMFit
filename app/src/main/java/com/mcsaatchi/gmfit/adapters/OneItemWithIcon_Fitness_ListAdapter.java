83aadac75684ca6bdaa4a9912930a88220c223a5
oday.maleh <oday.m@mcsaatchi.me>
2016-08-31 16:44:15 +0300

    Completely separate One Item With Icon List Adapter for the Fitness Widgets

---
+++ app/src/main/java/com/mcsaatchi/gmfit/adapters/OneItemWithIcon_Fitness_ListAdapter.java
@@ -0,0 +1,81 @@
+package com.mcsaatchi.gmfit.adapters;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.mcsaatchi.gmfit.R;
+import com.mcsaatchi.gmfit.models.FitnessWidget;
+
+import java.util.ArrayList;
+
+public class OneItemWithIcon_Fitness_ListAdapter extends BaseAdapter {
+
+    private Context context;
+    private ArrayList<FitnessWidget> widgetsMap;
+    private int drawableResId;
+
+    public OneItemWithIcon_Fitness_ListAdapter(Context context, ArrayList<FitnessWidget> widgetsMap, int drawableResId) {
+        super();
+        this.context = context;
+        this.widgetsMap = widgetsMap;
+        this.drawableResId = drawableResId;
+    }
+
+    public void notifyData() {
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getCount() {
+        return widgetsMap.size();
+    }
+
+    @Override
+    public Object getItem(int index) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+        if (convertView == null) {
+            LayoutInflater inflater = (LayoutInflater) context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.list_item_one_with_icon, parent,
+                    false);
+
+            holder = new ViewHolder();
+
+            holder.itemNameTV = (TextView) convertView.findViewById(R.id.itemNameTV);
+            holder.itemIconRightIMG = (ImageView) convertView.findViewById(R.id.itemIconRightIMG);
+
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        holder.itemIconRightIMG.setImageResource(drawableResId);
+        holder.itemNameTV.setText((widgetsMap.get(position)).getTitle());
+
+        Log.d("TAG", "getView: Name is : " + (widgetsMap.get(position)).getTitle());
+        Log.d("TAG", "getView: Name is : " + (widgetsMap.get(position)).getValue());
+
+        return convertView;
+    }
+
+    class ViewHolder {
+        TextView itemNameTV;
+        ImageView itemIconRightIMG;
+    }
+}
