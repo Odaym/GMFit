871504f26c4800e12647d53c5b07f6dbfa3ac807
oday.maleh <oday.m@mcsaatchi.me>
2016-08-31 16:39:19 +0300

    Attach fitness widgets and nutrition widgets separately when sending them off to the Customise Widgets Activity

--- app/src/main/java/com/mcsaatchi/gmfit/activities/CustomizeWidgetsAndCharts_Activity.java
+++ app/src/main/java/com/mcsaatchi/gmfit/activities/CustomizeWidgetsAndCharts_Activity.java
@@ -2,18 +2,22 @@ package com.mcsaatchi.gmfit.activities;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.design.internal.ParcelableSparseArray;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 
 import com.mcsaatchi.gmfit.R;
 import com.mcsaatchi.gmfit.classes.Cons;
 import com.mcsaatchi.gmfit.classes.SlidingTabLayout;
 import com.mcsaatchi.gmfit.fragments.CustomizeCharts_Fragment;
 import com.mcsaatchi.gmfit.fragments.CustomizeWidgets_Fragment;
+import com.mcsaatchi.gmfit.models.FitnessWidget;
+import com.mcsaatchi.gmfit.models.NutritionWidget;
+
+import java.util.ArrayList;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -26,8 +30,12 @@ public class CustomizeWidgetsAndCharts_Activity extends Base_Activity {
     @Bind(R.id.toolbar)
     Toolbar toolbar;
 
-    private String typeOfFragmentToCustomizeFor;
-    private ParcelableSparseArray widgetsMapExtra;
+    private CustomizerViewPager_Adapter fragmentsPagerAdapter;
+
+    private String typeOfFragmentToCustomiseFor;
+
+    private ArrayList<FitnessWidget> fitnessWidgetsMapExtra;
+    private ArrayList<NutritionWidget> nutritionWidgetsMapExtra;
 
     private String[] tabTitles = new String[]{
             "Widgets", "Charts"
@@ -47,11 +55,24 @@ public class CustomizeWidgetsAndCharts_Activity extends Base_Activity {
 
         //Grab the Fragment type from one of the three Fragments (Fitness, Nutrition, Health)
         if (intentExtras != null) {
-            typeOfFragmentToCustomizeFor = intentExtras.getString(Cons.EXTRAS_CUSTOMIZE_WIDGETS_FRAGMENT_TYPE);
-            widgetsMapExtra = (ParcelableSparseArray) intentExtras.getSparseParcelableArray(Cons.BUNDLE_FITNESS_WIDGETS_MAP);
+            typeOfFragmentToCustomiseFor = intentExtras.getString(Cons.EXTRAS_CUSTOMIZE_WIDGETS_CHARTS_FRAGMENT_TYPE);
+            Log.d("TAG", "onCreate: Extras not null");
+            if (typeOfFragmentToCustomiseFor != null) {
+                switch (typeOfFragmentToCustomiseFor) {
+                    case Cons.EXTRAS_FITNESS_FRAGMENT:
+                        fitnessWidgetsMapExtra = intentExtras.getParcelableArrayList(Cons.BUNDLE_FITNESS_WIDGETS_MAP);
+                        break;
+                    case Cons.EXTRAS_NUTRITION_FRAGMENT:
+                        nutritionWidgetsMapExtra = intentExtras.getParcelableArrayList(Cons.BUNDLE_NUTRITION_WIDGETS_MAP);
+                        break;
+                }
+
+
+                Log.d("TAG", "onCreate: Type of fragment being sent from " + typeOfFragmentToCustomiseFor);
+            }
         }
 
-        CustomizerViewPager_Adapter fragmentsPagerAdapter = new CustomizerViewPager_Adapter(getSupportFragmentManager(), tabTitles, widgetsMapExtra);
+        fragmentsPagerAdapter = new CustomizerViewPager_Adapter(getSupportFragmentManager(), tabTitles);
 
         tabs.setDistributeEvenly(true);
         tabs.setSelectedIndicatorColors(getResources().getColor(android.R.color.white));
@@ -65,20 +86,29 @@ public class CustomizeWidgetsAndCharts_Activity extends Base_Activity {
     public class CustomizerViewPager_Adapter extends FragmentStatePagerAdapter {
 
         private String[] tabTitles;
-        private ParcelableSparseArray widgetsMapExtra;
 
-        public CustomizerViewPager_Adapter(FragmentManager fm, String[] tabTitles, ParcelableSparseArray widgetsMapExtra) {
+        public CustomizerViewPager_Adapter(FragmentManager fm, String[] tabTitles) {
             super(fm);
 
             this.tabTitles = tabTitles;
-            this.widgetsMapExtra = widgetsMapExtra;
         }
 
         @Override
         public Fragment getItem(int position) {
             Bundle fragmentArguments = new Bundle();
-            fragmentArguments.putString(Cons.EXTRAS_CUSTOMIZE_WIDGETS_FRAGMENT_TYPE, typeOfFragmentToCustomizeFor);
-            fragmentArguments.putParcelable(Cons.BUNDLE_FITNESS_WIDGETS_MAP, widgetsMapExtra);
+
+            fragmentArguments.putString(Cons.EXTRAS_CUSTOMIZE_WIDGETS_CHARTS_FRAGMENT_TYPE, typeOfFragmentToCustomiseFor);
+
+            if (typeOfFragmentToCustomiseFor != null) {
+                switch (typeOfFragmentToCustomiseFor) {
+                    case Cons.EXTRAS_FITNESS_FRAGMENT:
+                        fragmentArguments.putParcelableArrayList(Cons.BUNDLE_FITNESS_WIDGETS_MAP, fitnessWidgetsMapExtra);
+                        break;
+                    case Cons.EXTRAS_NUTRITION_FRAGMENT:
+                        fragmentArguments.putParcelableArrayList(Cons.BUNDLE_NUTRITION_WIDGETS_MAP, nutritionWidgetsMapExtra);
+                        break;
+                }
+            }
 
             switch (position) {
                 case 0:
