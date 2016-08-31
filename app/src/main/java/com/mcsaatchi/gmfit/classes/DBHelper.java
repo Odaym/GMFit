8b34608a1ef29fe04916d6be9ce5caaae675ab7a
oday.maleh <oday.m@mcsaatchi.me>
2016-08-31 16:46:11 +0300

    Add DAO implementation for nutrition widget and fitness widgets database tables, plus add the first 4 fitness widgets manually at DB creation, later updated inside the sensor

--- app/src/main/java/com/mcsaatchi/gmfit/classes/DBHelper.java
+++ app/src/main/java/com/mcsaatchi/gmfit/classes/DBHelper.java
@@ -1,6 +1,7 @@
 package com.mcsaatchi.gmfit.classes;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.database.sqlite.SQLiteDatabase;
 import android.util.Log;
 
@@ -8,8 +9,11 @@ import com.j256.ormlite.android.apptools.OrmLiteSqliteOpenHelper;
 import com.j256.ormlite.dao.RuntimeExceptionDao;
 import com.j256.ormlite.support.ConnectionSource;
 import com.j256.ormlite.table.TableUtils;
+import com.mcsaatchi.gmfit.R;
 import com.mcsaatchi.gmfit.models.DataChart;
+import com.mcsaatchi.gmfit.models.FitnessWidget;
 import com.mcsaatchi.gmfit.models.MealItem;
+import com.mcsaatchi.gmfit.models.NutritionWidget;
 import com.mcsaatchi.gmfit.models.User;
 
 import java.sql.SQLException;
@@ -23,9 +27,8 @@ public class DBHelper extends OrmLiteSqliteOpenHelper {
     private static final String DATABASE_NAME = "GMFit.db";
 
     private static final int DATABASE_VERSION = 1;
-
     private static final String TAG = "DBHelper";
-
+    private Context context;
     private String[] mealTypes = new String[]{"Breakfast", "Lunch", "Dinner"};
 
     private String[] mealNames = new String[]{"Twice-Baked Broccoli-and-Kale-Stuffed Potatoes",
@@ -87,11 +90,14 @@ public class DBHelper extends OrmLiteSqliteOpenHelper {
             "Chocolate Chip Cookies", "Waffer Biscuits", "Nuts and crackers"};
 
     private RuntimeExceptionDao<MealItem, Integer> mealItemRunTimeDAO = null;
+    private RuntimeExceptionDao<FitnessWidget, Integer> fitnessWidgetsRunTimeDAO = null;
+    private RuntimeExceptionDao<NutritionWidget, Integer> nutritionWidgetsRunTimeDAO = null;
     private RuntimeExceptionDao<DataChart, Integer> dataChartRunTimeDAO = null;
     private RuntimeExceptionDao<User, Integer> userRunTimeDAO = null;
 
     public DBHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        this.context = context;
     }
 
     @Override
@@ -100,8 +106,12 @@ public class DBHelper extends OrmLiteSqliteOpenHelper {
             TableUtils.createTable(connectionSource, MealItem.class);
             TableUtils.createTable(connectionSource, DataChart.class);
             TableUtils.createTable(connectionSource, User.class);
+            TableUtils.createTable(connectionSource, FitnessWidget.class);
+            TableUtils.createTable(connectionSource, NutritionWidget.class);
 
             Log.d(TAG, "DBHelper: We're here in the DATABASE constructor");
+
+            SharedPreferences prefs = context.getSharedPreferences(Cons.SHARED_PREFS_TITLE, Context.MODE_PRIVATE);
 
             for (int i = 0; i < mealNames.length; i++) {
                 getMealItemDAO().create(new MealItem(mealNames[new Random().nextInt(mealNames.length)], mealTypes[new Random().nextInt(mealTypes.length)]));
@@ -110,6 +120,17 @@ public class DBHelper extends OrmLiteSqliteOpenHelper {
             for (int i = 0; i < snackNames.length; i++){
                 getMealItemDAO().create(new MealItem(snackNames[i], "Snacks"));
             }
+
+            FitnessWidget fw1 = new FitnessWidget("Walking", "Km/hour", 0, R.drawable.ic_running, 0);
+            FitnessWidget fw2 = new FitnessWidget("Biking", "meters", 0, R.drawable.ic_biking, 1);
+            FitnessWidget fw3 = new FitnessWidget("Steps", "steps", 0, R.drawable.ic_steps, 2);
+            FitnessWidget fw4 = new FitnessWidget("Calories", "Calories", 0, R.drawable.ic_calories, 3);
+
+            getFitnessWidgetsDAO().create(fw1);
+            getFitnessWidgetsDAO().create(fw2);
+            getFitnessWidgetsDAO().create(fw3);
+            getFitnessWidgetsDAO().create(fw4);
+
         } catch (SQLException e) {
             Log.e(DBHelper.class.getName(), "Can't create database", e);
             throw new RuntimeException(e);
@@ -126,6 +147,20 @@ public class DBHelper extends OrmLiteSqliteOpenHelper {
             mealItemRunTimeDAO = getRuntimeExceptionDao(MealItem.class);
         }
         return mealItemRunTimeDAO;
+    }
+
+    public RuntimeExceptionDao<FitnessWidget, Integer> getFitnessWidgetsDAO() {
+        if (fitnessWidgetsRunTimeDAO == null) {
+            fitnessWidgetsRunTimeDAO = getRuntimeExceptionDao(FitnessWidget.class);
+        }
+        return fitnessWidgetsRunTimeDAO;
+    }
+
+    public RuntimeExceptionDao<NutritionWidget, Integer> getNutritionWidgetsDAO() {
+        if (nutritionWidgetsRunTimeDAO == null) {
+            nutritionWidgetsRunTimeDAO = getRuntimeExceptionDao(NutritionWidget.class);
+        }
+        return nutritionWidgetsRunTimeDAO;
     }
 
     public RuntimeExceptionDao<DataChart, Integer> getDataChartDAO() {
@@ -146,5 +181,9 @@ public class DBHelper extends OrmLiteSqliteOpenHelper {
     public void close() {
         super.close();
         mealItemRunTimeDAO = null;
+        dataChartRunTimeDAO = null;
+        userRunTimeDAO = null;
+        nutritionWidgetsRunTimeDAO = null;
+        fitnessWidgetsRunTimeDAO = null;
     }
 }
