0a530db35f54818f4af220876820da40d7835605
oday.maleh <oday.m@mcsaatchi.me>
2016-09-07 14:39:52 +0300

    New adapter implementation for viewing the list of meals available for adding. This one implements a Pinned Section Header List View and differentiates between a SECTION viewtype and an ITEM viewtype, changes layouts accordingly. Pretty good stuff!

---
+++ app/src/main/java/com/mcsaatchi/gmfit/adapters/SimpleSectioned_ListAdapter.java
@@ -0,0 +1,103 @@
+package com.mcsaatchi.gmfit.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.mcsaatchi.gmfit.R;
+import com.mcsaatchi.gmfit.models.MealItem;
+
+import java.util.List;
+
+import de.halfbit.pinnedsection.PinnedSectionListView;
+
+public class SimpleSectioned_ListAdapter extends BaseAdapter implements PinnedSectionListView.PinnedSectionListAdapter {
+
+    private static final int SECTION_VIEWTYPE = 1;
+    private static final int ITEM_VIEWTYPE = 2;
+    private Context context;
+    private List<MealItem> mealItems;
+
+    public SimpleSectioned_ListAdapter(Context context, List<MealItem> mealItems) {
+        this.context = context;
+        this.mealItems = mealItems;
+    }
+
+    @Override
+    public int getCount() {
+        return mealItems.size();
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return 3;
+    }
+
+    @Override
+    public MealItem getItem(int index) {
+        return mealItems.get(index);
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return getItem(position).getSectionType();
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+        if (convertView == null) {
+            LayoutInflater inflater = (LayoutInflater) context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+            holder = new ViewHolder();
+
+            if (mealItems.get(position).getSectionType() == SECTION_VIEWTYPE) {
+                convertView = inflater.inflate(android.R.layout.simple_list_item_1, parent, false);
+
+                holder.itemNameTV = (TextView) convertView.findViewById(android.R.id.text1);
+            } else {
+                convertView = inflater.inflate(R.layout.list_item_one_with_icon, parent,
+                        false);
+
+                holder.itemNameTV = (TextView) convertView.findViewById(R.id.itemNameTV);
+                holder.itemIconRightIMG = (ImageView) convertView.findViewById(R.id.itemIconRightIMG);
+            }
+
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        holder.itemNameTV.setText(mealItems.get(position).getName());
+
+        if (mealItems.get(position).getSectionType() == ITEM_VIEWTYPE) {
+            holder.itemNameTV.setTextColor(context.getResources().getColor(android.R.color.black));
+            holder.itemIconRightIMG.setImageResource(R.drawable.ic_arrow_right_pink);
+        } else {
+            holder.itemNameTV.setTextColor(context.getResources().getColor(android.R.color.white));
+            holder.itemNameTV.setBackgroundColor(context.getResources().getColor(R.color.nutrition_red));
+        }
+
+        return convertView;
+    }
+
+    @Override
+    public boolean isItemViewTypePinned(int viewType) {
+        return viewType == SECTION_VIEWTYPE;
+    }
+
+    class ViewHolder {
+        TextView itemNameTV;
+        ImageView itemIconRightIMG;
+    }
+}
