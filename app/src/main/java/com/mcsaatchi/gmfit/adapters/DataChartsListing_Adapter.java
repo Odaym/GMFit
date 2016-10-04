c8f7af2607dd28774bd846921c9ef38d478a384e
oday.maleh <oday.m@mcsaatchi.me>
2016-10-04 13:13:10 +0300

    No more SparseArray to describe Nutritional Facts list and Available Charts list, now just using corresponding class objects to do this, changed corresponding adapter and added a new one for Nutritional Facts list

--- app/src/main/java/com/mcsaatchi/gmfit/adapters/DataChartsListing_Adapter.java
+++ app/src/main/java/com/mcsaatchi/gmfit/adapters/DataChartsListing_Adapter.java
@@ -1,7 +1,6 @@
 package com.mcsaatchi.gmfit.adapters;
 
 import android.content.Context;
-import android.util.SparseArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,26 +8,29 @@ import android.widget.BaseAdapter;
 import android.widget.TextView;
 
 import com.mcsaatchi.gmfit.R;
+import com.mcsaatchi.gmfit.models.DataChart;
 
-public class TwoItem_Sparse_ListAdapter extends BaseAdapter {
+import java.util.List;
+
+public class DataChartsListing_Adapter extends BaseAdapter {
 
     private Context context;
-    private SparseArray<String[]> listItems;
+    private List<DataChart> chartListItems;
 
-    public TwoItem_Sparse_ListAdapter(Context context, SparseArray<String[]> listItems) {
+    public DataChartsListing_Adapter(Context context, List<DataChart> chartListItems) {
         super();
         this.context = context;
-        this.listItems = listItems;
+        this.chartListItems = chartListItems;
     }
 
     @Override
     public int getCount() {
-        return listItems.size();
+        return chartListItems.size();
     }
 
     @Override
-    public String[] getItem(int index) {
-        return listItems.get(index);
+    public DataChart getItem(int index) {
+        return chartListItems.get(index);
     }
 
     @Override
@@ -55,8 +57,9 @@ public class TwoItem_Sparse_ListAdapter extends BaseAdapter {
             holder = (ViewHolder) convertView.getTag();
         }
 
-        holder.itemNameTV.setText(listItems.get(position)[0]);
-        holder.itemHintTV.setText(listItems.get(position)[1]);
+        holder.itemNameTV.setText(chartListItems.get(position).getName());
+
+        holder.itemHintTV.setText(chartListItems.get(position).getMeasurementUnit());
 
         return convertView;
     }
