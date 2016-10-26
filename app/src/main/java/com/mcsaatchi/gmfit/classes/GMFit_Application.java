d5a72c57eaf7f21218ba74b2198ffd6082a1c224
oday.maleh <oday.m@mcsaatchi.me>
2016-10-26 16:52:23 +0300

    Preliminary offline caching solution on all 3 sections

--- app/src/main/java/com/mcsaatchi/gmfit/classes/GMFit_Application.java
+++ app/src/main/java/com/mcsaatchi/gmfit/classes/GMFit_Application.java
@@ -1,6 +1,9 @@
 package com.mcsaatchi.gmfit.classes;
 
 import android.app.Application;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 
 import com.crashlytics.android.Crashlytics;
 
@@ -8,10 +11,28 @@ import io.fabric.sdk.android.Fabric;
 
 public class GMFit_Application extends Application {
 
+    private static GMFit_Application instance;
+
+    public static boolean hasNetwork() {
+        return instance.checkIfHasNetwork();
+    }
+
+    public static GMFit_Application getInstance() {
+        return instance;
+    }
+
     @Override
     public void onCreate() {
         super.onCreate();
 
+        instance = this;
+
         Fabric.with(this, new Crashlytics());
+    }
+
+    public boolean checkIfHasNetwork() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = cm.getActiveNetworkInfo();
+        return networkInfo != null && networkInfo.isConnected();
     }
 }
