277cffa8163c5bcf5a165d7b19be37385947cffb
oday.maleh <oday.m@mcsaatchi.me>
2016-10-27 17:23:55 +0300

    Delete a user meal (API request) and visually, when the user swipes on one of the meals

--- app/src/main/java/com/mcsaatchi/gmfit/adapters/UserMeals_RecyclerAdapterDragSwipe.java
+++ app/src/main/java/com/mcsaatchi/gmfit/adapters/UserMeals_RecyclerAdapterDragSwipe.java
@@ -2,7 +2,9 @@ package com.mcsaatchi.gmfit.adapters;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,21 +13,32 @@ import android.widget.TextView;
 import com.mcsaatchi.gmfit.R;
 import com.mcsaatchi.gmfit.activities.SpecifyMealAmount_Activity;
 import com.mcsaatchi.gmfit.classes.Constants;
+import com.mcsaatchi.gmfit.classes.EventBus_Poster;
+import com.mcsaatchi.gmfit.classes.EventBus_Singleton;
+import com.mcsaatchi.gmfit.data_access.DataAccessHandler;
 import com.mcsaatchi.gmfit.models.MealItem;
+import com.mcsaatchi.gmfit.rest.DefaultGetResponse;
 import com.mcsaatchi.gmfit.touch_helpers.Drag_Swipe_ItemTouchHelperAdapter;
 
-import java.util.Collections;
 import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class UserMeals_RecyclerAdapterDragSwipe extends RecyclerView.Adapter<UserMeals_RecyclerAdapterDragSwipe.MyViewHolder>
         implements Drag_Swipe_ItemTouchHelperAdapter {
 
     private List<MealItem> mealItems;
     private Context context;
+    private SharedPreferences prefs;
 
     public UserMeals_RecyclerAdapterDragSwipe(Context context, List<MealItem> mealItems) {
         this.mealItems = mealItems;
         this.context = context;
+
+        if (context != null)
+            prefs = context.getSharedPreferences(Constants.SHARED_PREFS_TITLE, Context.MODE_PRIVATE);
     }
 
     @Override
@@ -58,31 +71,43 @@ public class UserMeals_RecyclerAdapterDragSwipe extends RecyclerView.Adapter<Use
     }
 
     @Override
-    public void onItemMove(int fromPosition, int toPosition) {
-        if (fromPosition < toPosition) {
-            for (int i = fromPosition; i < toPosition; i++) {
-                Collections.swap(mealItems, i, i + 1);
-            }
-        } else {
-            for (int i = fromPosition; i > toPosition; i--) {
-                Collections.swap(mealItems, i, i - 1);
-            }
-        }
-
-        notifyItemMoved(fromPosition, toPosition);
-    }
-
-    @Override
     public void onItemDismiss(int position) {
-        notifyItemRemoved(position);
 
         try {
+            deleteUserMeal(mealItems.get(position).getInstance_id());
+
             mealItems.remove(position);
+            notifyItemRemoved(position);
+            notifyItemRangeChanged(position, mealItems.size());
         } catch (IndexOutOfBoundsException ignored) {
         }
     }
 
-    class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+    private void deleteUserMeal(int instance_id) {
+        DataAccessHandler.getInstance().deleteUserMeal(prefs.getString(Constants.PREF_USER_ACCESS_TOKEN, Constants.NO_ACCESS_TOKEN_FOUND_IN_PREFS),
+                instance_id, new Callback<DefaultGetResponse>() {
+                    @Override
+                    public void onResponse(Call<DefaultGetResponse> call, Response<DefaultGetResponse> response) {
+                        Log.d("TAG", "onResponse: Response code was : " + response.code());
+
+                        switch (response.code()) {
+                            case 200:
+                                Log.d("TAG", "onResponse: Meal item removed!");
+
+                                EventBus_Singleton.getInstance().post(new EventBus_Poster(Constants.EXTRAS_DELETED_MEAL_ENTRY));
+
+                                break;
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<DefaultGetResponse> call, Throwable t) {
+                        Log.d("TAG", "onFailure: FAilure");
+                    }
+                });
+    }
+
+    class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         TextView entryTitleTV, entryDescriptionTV, entryUnitsTV;
 
         MyViewHolder(View view) {
