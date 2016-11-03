18d69460652d9c6ce63d401e85aac4c5e4c25a70
oday.maleh <oday.m@mcsaatchi.me>
2016-11-03 12:38:20 +0200

    Implement getActivityLevels and getUserGoals in their respective fragments, move the 3rd fragment to the 4th

--- app/src/main/java/com/mcsaatchi/gmfit/fragments/Setup_Profile_2_Fragment.java
+++ app/src/main/java/com/mcsaatchi/gmfit/fragments/Setup_Profile_2_Fragment.java
@@ -1,29 +1,37 @@
 package com.mcsaatchi.gmfit.fragments;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.CompoundButton;
 import android.widget.RadioButton;
 
 import com.mcsaatchi.gmfit.R;
 import com.mcsaatchi.gmfit.classes.Constants;
+import com.mcsaatchi.gmfit.data_access.DataAccessHandler;
+import com.mcsaatchi.gmfit.rest.UserGoalsResponse;
+import com.mcsaatchi.gmfit.rest.UserGoalsResponseBody;
+
+import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import worker8.com.github.radiogroupplus.RadioGroupPlus;
 
 public class Setup_Profile_2_Fragment extends Fragment {
 
-    @Bind(R.id.maintainWeightRdBTN)
-    RadioButton maintainWeightRdBTN;
-    @Bind(R.id.loseWeightRdBTN)
-    RadioButton loseWeightRdBTN;
-    @Bind(R.id.gainWeightRdBTN)
-    RadioButton gainWeightRdBTN;
+    @Bind(R.id.goalRadioButtonsGroup)
+    RadioGroupPlus goalRadioButtonsGroup;
 
     private SharedPreferences prefs;
 
@@ -38,33 +46,58 @@ public class Setup_Profile_2_Fragment extends Fragment {
 
         prefs = getActivity().getSharedPreferences(Constants.SHARED_PREFS_TITLE, Context.MODE_PRIVATE);
 
-        prefs.edit().putString(Constants.EXTRAS_USER_PROFILE_GOAL, "Lose weight").apply();
-
-        loseWeightRdBTN.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean checked) {
-                if (checked)
-                    prefs.edit().putString(Constants.EXTRAS_USER_PROFILE_GOAL, "Lose weight").apply();
-            }
-        });
-
-        maintainWeightRdBTN.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean checked) {
-                if (checked)
-                    prefs.edit().putString(Constants.EXTRAS_USER_PROFILE_GOAL, "Maintain weight").apply();
-            }
-        });
-
-
-        gainWeightRdBTN.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean checked) {
-                if (checked)
-                    prefs.edit().putString(Constants.EXTRAS_USER_PROFILE_GOAL, "Gain weight").apply();
-            }
-        });
+        getUserGoals();
 
         return fragmentView;
+    }
+
+    private void getUserGoals() {
+        final AlertDialog alertDialog = new AlertDialog.Builder(getActivity()).create();
+        alertDialog.setTitle(R.string.fetching_user_goals);
+        alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, getString(R.string.ok),
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+
+        DataAccessHandler.getInstance().getUserGoals(prefs.getString(Constants.PREF_USER_ACCESS_TOKEN, Constants.NO_ACCESS_TOKEN_FOUND_IN_PREFS),
+                new Callback<UserGoalsResponse>() {
+                    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+                    @Override
+                    public void onResponse(Call<UserGoalsResponse> call, Response<UserGoalsResponse> response) {
+                        switch (response.code()) {
+                            case 200:
+
+                                List<UserGoalsResponseBody> activityLevels = response.body().getData().getBody();
+
+                                for (int i = 0; i < activityLevels.size(); i++) {
+                                    View listItemRadioButton = getActivity().getLayoutInflater().inflate(R.layout.user_goals_list_item_radio_button_row, null);
+
+                                    final RadioButton radioButtonItem = (RadioButton) listItemRadioButton.findViewById(R.id.goalRadioButtonItem);
+                                    radioButtonItem.setText(activityLevels.get(i).getName());
+                                    radioButtonItem.setId(activityLevels.get(i).getId());
+
+                                    radioButtonItem.setOnClickListener(new View.OnClickListener() {
+                                        @Override
+                                        public void onClick(View view) {
+                                            prefs.edit().putInt(Constants.EXTRAS_USER_PROFILE_GOAL_ID, radioButtonItem.getId()).apply();
+                                            prefs.edit().putString(Constants.EXTRAS_USER_PROFILE_GOAL, radioButtonItem.getText().toString()).apply();
+                                        }
+                                    });
+
+                                    goalRadioButtonsGroup.addView(listItemRadioButton);
+                                }
+
+                                break;
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<UserGoalsResponse> call, Throwable t) {
+                        alertDialog.setMessage(getString(R.string.error_response_from_server_incorrect));
+                        alertDialog.show();
+                    }
+                });
     }
 }
--- app/src/main/java/com/mcsaatchi/gmfit/fragments/Setup_Profile_3_Fragment.java
+++ app/src/main/java/com/mcsaatchi/gmfit/fragments/Setup_Profile_3_Fragment.java
@@ -1,44 +1,25 @@
 package com.mcsaatchi.gmfit.fragments;
 
-import android.app.Activity;
-import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.inputmethod.InputMethodManager;
-import android.widget.AdapterView;
-import android.widget.Spinner;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
-import com.andreabaccega.widget.FormEditText;
-import com.codetroopers.betterpickers.calendardatepicker.CalendarDatePickerDialogFragment;
 import com.mcsaatchi.gmfit.R;
-import com.mcsaatchi.gmfit.activities.Main_Activity;
-import com.mcsaatchi.gmfit.adapters.MedicalConditionsSpinnerAdapter;
 import com.mcsaatchi.gmfit.classes.Constants;
-import com.mcsaatchi.gmfit.classes.EventBus_Poster;
-import com.mcsaatchi.gmfit.classes.EventBus_Singleton;
-import com.mcsaatchi.gmfit.classes.Helpers;
 import com.mcsaatchi.gmfit.data_access.DataAccessHandler;
-import com.mcsaatchi.gmfit.rest.AuthenticationResponseChart;
-import com.mcsaatchi.gmfit.rest.AuthenticationResponseWidget;
-import com.mcsaatchi.gmfit.rest.DefaultGetResponse;
-import com.mcsaatchi.gmfit.rest.MedicalConditionsResponse;
-import com.mcsaatchi.gmfit.rest.MedicalConditionsResponseDatum;
-import com.mcsaatchi.gmfit.rest.UiResponse;
-import com.squareup.otto.Subscribe;
+import com.mcsaatchi.gmfit.rest.ActivityLevelsResponse;
+import com.mcsaatchi.gmfit.rest.ActivityLevelsResponseBody;
 
-import java.text.DateFormatSymbols;
-import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.List;
 
 import butterknife.Bind;
@@ -46,46 +27,13 @@ import butterknife.ButterKnife;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
+import worker8.com.github.radiogroupplus.RadioGroupPlus;
 
-public class Setup_Profile_3_Fragment extends Fragment implements CalendarDatePickerDialogFragment.OnDateSetListener {
+public class Setup_Profile_3_Fragment extends Fragment {
 
-    private static final String FRAG_TAG_DATE_PICKER = "fragment_date_picker_name";
-    private static final String TAG = "Setup_Profile_3_Fragment";
-
-    @Bind(R.id.dateOfBirthTV)
-    TextView dateOfBirthTV;
-    @Bind(R.id.weightET)
-    FormEditText weightET;
-    @Bind(R.id.heightET)
-    FormEditText heightET;
-    @Bind(R.id.bloodTypeSpinner)
-    Spinner bloodTypeSpinner;
-    @Bind(R.id.genderSpinner)
-    Spinner genderSpinner;
-    @Bind(R.id.medicalConditionsSpinner)
-    Spinner medicalConditionsSpinner;
-
+    @Bind(R.id.activityLevelsRadioButtonsGroup)
+    RadioGroupPlus activityLevelsRadioButtonsGroup;
     private SharedPreferences prefs;
-    private ArrayList<FormEditText> allFields = new ArrayList<>();
-    private String dateOfBirth = "";
-
-    private InputMethodManager inputMethodManager;
-
-    private ArrayList<String> bloodTypeItems = new ArrayList<String>() {{
-        add("O-");
-        add("O+");
-        add("A-");
-        add("A+");
-        add("B-");
-        add("B+");
-        add("AB-");
-        add("AB+");
-    }};
-
-    private ArrayList<String> genderItems = new ArrayList<String>() {{
-        add("Male");
-        add("Female");
-    }};
 
     @Override
     public View onCreateView(LayoutInflater inflater,
@@ -96,241 +44,63 @@ public class Setup_Profile_3_Fragment extends Fragment implements CalendarDatePi
 
         ButterKnife.bind(this, fragmentView);
 
-        //When going back and forth within the 3 Setup Profile fragments, don't reregister the Eventbus
-        try {
-            EventBus_Singleton.getInstance().register(this);
-        } catch (IllegalArgumentException ignored) {
-        }
-
-        initCustomSpinner(genderItems, genderSpinner);
-
-        initCustomSpinner(bloodTypeItems, bloodTypeSpinner);
-
         prefs = getActivity().getSharedPreferences(Constants.SHARED_PREFS_TITLE, Context.MODE_PRIVATE);
 
-        getAndPopulateMedicalConditions();
-
-        inputMethodManager = (InputMethodManager) getActivity().getSystemService(Activity.INPUT_METHOD_SERVICE);
-
-        allFields.add(weightET);
-        allFields.add(heightET);
-
-        dateOfBirthTV.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                CalendarDatePickerDialogFragment cdp = new CalendarDatePickerDialogFragment()
-                        .setOnDateSetListener(Setup_Profile_3_Fragment.this)
-                        .setFirstDayOfWeek(Calendar.MONDAY)
-                        .setDoneText(getString(R.string.accept_ok))
-                        .setCancelText(getString(R.string.decline_cancel))
-                        .setPreselectedDate(2000, 0, 1)
-                        .setThemeLight();
-                cdp.show(getActivity().getSupportFragmentManager(), FRAG_TAG_DATE_PICKER);
-            }
-        });
+        getActivityLevels();
 
         return fragmentView;
     }
 
-    @Override
-    public void onDateSet(CalendarDatePickerDialogFragment dialog, int year, int monthOfYear, int dayOfMonth) {
-        dateOfBirthTV.setText(new DateFormatSymbols().getMonths()[monthOfYear] + " " + dayOfMonth + ", " + year);
-    }
-
-    @Subscribe
-    public void handle_BusEvents(EventBus_Poster ebp) {
-        String ebpMessage = ebp.getMessage();
-
-        switch (ebpMessage) {
-            case Constants.EVENT_USER_FINALIZE_SETUP_PROFILE:
-                if (!weightET.getText().toString().isEmpty() && !heightET.getText().toString().isEmpty()) {
-                    int finalGender;
-
-                    finalGender = genderSpinner.getSelectedItem().toString().equals("Male") ? 1 : 0;
-
-                    String finalDateOfBirth;
-
-                    if (!dateOfBirth.isEmpty())
-                        finalDateOfBirth = dateOfBirth;
-                    else
-                        finalDateOfBirth = "1990-01-01";
-
-                    float finalWeight = Float.parseFloat(weightET.getText().toString());
-                    float finalHeight = Float.parseFloat(heightET.getText().toString());
-
-                    String finalBloodType = bloodTypeSpinner.getSelectedItem().toString();
-
-                    prefs.edit().putString(Constants.EXTRAS_USER_PROFILE_DATE_OF_BIRTH, finalDateOfBirth).apply();
-                    prefs.edit().putInt(Constants.EXTRAS_USER_PROFILE_GENDER, finalGender).apply();
-                    prefs.edit().putFloat(Constants.EXTRAS_USER_PROFILE_WEIGHT, finalWeight).apply();
-                    prefs.edit().putFloat(Constants.EXTRAS_USER_PROFILE_HEIGHT, finalHeight).apply();
-                    prefs.edit().putString(Constants.EXTRAS_USER_PROFILE_BLOOD_TYPE, finalBloodType).apply();
-
-                    /**
-                     * These values are from previous steps 1 and 2 in the Setup Profile process
-                     */
-                    String nationality = prefs.getString(Constants.EXTRAS_USER_PROFILE_NATIONALITY, "");
-                    String measurementSystem = prefs.getString(Constants.EXTRAS_USER_PROFILE_MEASUREMENT_SYSTEM, "");
-                    String goal = prefs.getString(Constants.EXTRAS_USER_PROFILE_GOAL, "");
-
-                    setupUserProfile(finalDateOfBirth, finalBloodType, nationality, (int) medicalConditionsSpinner.getSelectedItemId(), measurementSystem, goal,
-                            finalGender, finalHeight, finalWeight, Helpers.calculateBMI(finalWeight, finalHeight));
-                } else {
-//                    AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-//                    builder.setTitle(R.string.weight_or_height_empty_dialog_title)
-//                            .setMessage(R.string.weight_or_height_empty_dialog_message)
-//                            .setCancelable(false)
-//                            .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
-//                                public void onClick(DialogInterface dialog, int id) {
-//                                    //do things
-//                                }
-//                            });
-//                    AlertDialog alert = builder.create();
-//                    alert.show();
-                }
-
-                break;
-        }
-    }
-
-    private void getAndPopulateMedicalConditions() {
-        DataAccessHandler.getInstance().getMedicalConditions(prefs.getString(Constants.PREF_USER_ACCESS_TOKEN, Constants.NO_ACCESS_TOKEN_FOUND_IN_PREFS), new Callback<MedicalConditionsResponse>() {
-            @Override
-            public void onResponse(Call<MedicalConditionsResponse> call, Response<MedicalConditionsResponse> response) {
-                switch (response.code()) {
-                    case 200:
-                        ArrayList<MedicalConditionsResponseDatum> allMedicalData = (ArrayList<MedicalConditionsResponseDatum>) response.body().getData().getBody().getData();
-
-                        ArrayList<String> medicalConditions = new ArrayList<>();
-
-                        medicalConditions.add("None");
-
-                        for (int i = 0; i < allMedicalData.size(); i++) {
-                            medicalConditions.add(allMedicalData.get(i).getName());
-                        }
-
-                        initCustomSpinner(medicalConditions, medicalConditionsSpinner);
-
-                        break;
-                    case 401:
-
-                        break;
-                }
-            }
-
-            @Override
-            public void onFailure(Call<MedicalConditionsResponse> call, Throwable t) {
-
-            }
-        });
-    }
-
-    private void setupUserProfile(String finalDateOfBirth, String bloodType, String nationality, int medical_condition, String measurementSystem, String goal,
-                                  int finalGender, double height, double weight, double BMI) {
-
-        final ProgressDialog waitingDialog = new ProgressDialog(getActivity());
-        waitingDialog.setTitle(getString(R.string.signing_up_dialog_title));
-        waitingDialog.setMessage(getString(R.string.please_wait_dialog_message));
-        waitingDialog.show();
-
+    private void getActivityLevels() {
         final AlertDialog alertDialog = new AlertDialog.Builder(getActivity()).create();
-        alertDialog.setTitle(R.string.signing_up_dialog_title);
+        alertDialog.setTitle(R.string.fetching_activity_levels_dialog_title);
         alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, getString(R.string.ok),
                 new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
                         dialog.dismiss();
-
-                        if (waitingDialog.isShowing())
-                            waitingDialog.dismiss();
                     }
                 });
 
-        DataAccessHandler.getInstance().updateUserProfile(prefs.getString(Constants.PREF_USER_ACCESS_TOKEN, Constants.NO_ACCESS_TOKEN_FOUND_IN_PREFS), finalDateOfBirth,
-                bloodType, nationality, medical_condition, measurementSystem, goal, finalGender, height, weight, BMI, new Callback<DefaultGetResponse>() {
+        DataAccessHandler.getInstance().getActivityLevels(prefs.getString(Constants.PREF_USER_ACCESS_TOKEN, Constants.NO_ACCESS_TOKEN_FOUND_IN_PREFS),
+                new Callback<ActivityLevelsResponse>() {
+                    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
                     @Override
-                    public void onResponse(Call<DefaultGetResponse> call, Response<DefaultGetResponse> response) {
+                    public void onResponse(Call<ActivityLevelsResponse> call, Response<ActivityLevelsResponse> response) {
                         switch (response.code()) {
                             case 200:
-                                getUiForSection(waitingDialog, "fitness");
-                                break;
-                            case 401:
-                                alertDialog.setMessage(getString(R.string.login_failed_wrong_credentials));
-                                alertDialog.show();
+
+                                List<ActivityLevelsResponseBody> activityLevels = response.body().getData().getBody();
+
+                                for (int i = 0; i < activityLevels.size(); i++) {
+                                    View listItemRadioButton = getActivity().getLayoutInflater().inflate(R.layout.activity_levels_list_item_radio_button_row, null);
+
+                                    final RadioButton radioButtonItem = (RadioButton) listItemRadioButton.findViewById(R.id.activityLevelRadioButtonItem);
+                                    radioButtonItem.setText(activityLevels.get(i).getName());
+                                    radioButtonItem.setId(activityLevels.get(i).getId());
+
+                                    TextView radioButtonHintTV = (TextView) listItemRadioButton.findViewById(R.id.radioButtonHintTV);
+                                    radioButtonHintTV.setText(activityLevels.get(i).getDescription());
+
+                                    radioButtonItem.setOnClickListener(new View.OnClickListener() {
+                                        @Override
+                                        public void onClick(View view) {
+                                            prefs.edit().putInt(Constants.EXTRAS_USER_PROFILE_ACTIVITY_LEVEL_ID, radioButtonItem.getId()).apply();
+                                            prefs.edit().putString(Constants.EXTRAS_USER_PROFILE_ACTIVITY_LEVEL, radioButtonItem.getText().toString()).apply();
+                                        }
+                                    });
+
+                                    activityLevelsRadioButtonsGroup.addView(listItemRadioButton);
+                                }
+
                                 break;
                         }
                     }
 
                     @Override
-                    public void onFailure(Call<DefaultGetResponse> call, Throwable t) {
+                    public void onFailure(Call<ActivityLevelsResponse> call, Throwable t) {
                         alertDialog.setMessage(getString(R.string.error_response_from_server_incorrect));
                         alertDialog.show();
                     }
                 });
-    }
-
-    private void getUiForSection(final ProgressDialog waitingDialog, String section) {
-        DataAccessHandler.getInstance().getUiForSection(prefs.getString(Constants.PREF_USER_ACCESS_TOKEN,
-                Constants.NO_ACCESS_TOKEN_FOUND_IN_PREFS), "http://gmfit.mcsaatchi.me/api/v1/user/ui?section=" + section, new Callback<UiResponse>() {
-            @Override
-            public void onResponse(Call<UiResponse> call, Response<UiResponse> response) {
-                switch (response.code()) {
-                    case 200:
-                        waitingDialog.dismiss();
-
-                        prefs.edit().putBoolean(Constants.EXTRAS_USER_LOGGED_IN, true).apply();
-                        prefs.edit().putBoolean(prefs.getString(Constants.EXTRAS_USER_EMAIL, "") + "_" + Constants.EVENT_FINISHED_SETTING_UP_PROFILE_SUCCESSFULLY, true).apply();
-
-                        List<AuthenticationResponseWidget> widgetsMap = response.body().getData().getBody().getWidgets();
-                        List<AuthenticationResponseChart> chartsMap = response.body().getData().getBody().getCharts();
-
-                        Intent intent = new Intent(getActivity(), Main_Activity.class);
-                        intent.putParcelableArrayListExtra(Constants.BUNDLE_FITNESS_WIDGETS_MAP, (ArrayList<AuthenticationResponseWidget>) widgetsMap);
-                        intent.putParcelableArrayListExtra(Constants.BUNDLE_FITNESS_CHARTS_MAP, (ArrayList<AuthenticationResponseChart>) chartsMap);
-                        startActivity(intent);
-
-                        getActivity().finish();
-
-                        break;
-                }
-            }
-
-            @Override
-            public void onFailure(Call<UiResponse> call, Throwable t) {
-
-            }
-        });
-    }
-
-    private void initCustomSpinner(ArrayList<String> listItems, Spinner spinner) {
-        MedicalConditionsSpinnerAdapter medicalConditionsSpinnerAdapter = new MedicalConditionsSpinnerAdapter(getActivity(), listItems);
-        spinner.setAdapter(medicalConditionsSpinnerAdapter);
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
-                String item = parent.getItemAtPosition(position).toString();
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
-
-        spinner.setOnTouchListener(new View.OnTouchListener() {
-
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                inputMethodManager.hideSoftInputFromWindow(weightET.getWindowToken(), 0);
-
-                return false;
-            }
-        });
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        EventBus_Singleton.getInstance().unregister(this);
     }
 }
