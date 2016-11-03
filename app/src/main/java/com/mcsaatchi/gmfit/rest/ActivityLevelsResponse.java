81d34f8432765bb5fb6d6c3540dac44e8137b4ed
oday.maleh <oday.m@mcsaatchi.me>
2016-11-03 12:36:20 +0200

    Disabled caching for now

---
+++ app/src/main/java/com/mcsaatchi/gmfit/rest/ActivityLevelsResponse.java
@@ -0,0 +1,25 @@
+package com.mcsaatchi.gmfit.rest;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class ActivityLevelsResponse {
+
+    @SerializedName("data")
+    @Expose
+    private ActivityLevelsResponseData data;
+
+    /**
+     * @return The data
+     */
+    public ActivityLevelsResponseData getData() {
+        return data;
+    }
+
+    /**
+     * @param data The data
+     */
+    public void setData(ActivityLevelsResponseData data) {
+        this.data = data;
+    }
+}
