71ac2d2afc9d07f24f29708ba03e9c88fa35f87c
oday.maleh <oday.m@mcsaatchi.me>
2016-11-18 12:59:05 +0200

    Activity for showing data like Privacy Policy and Terms of Use webviews

--- app/src/main/java/com/mcsaatchi/gmfit/activities/MetaTexts_Activity.java
+++ app/src/main/java/com/mcsaatchi/gmfit/activities/MetaTexts_Activity.java
@@ -10,41 +10,43 @@ import butterknife.ButterKnife;
 import com.mcsaatchi.gmfit.R;
 import com.mcsaatchi.gmfit.classes.Constants;
 
-public class UserPolicy_Activity extends Base_Activity {
-  @Bind(R.id.userPolicyWebView) WebView userPolicyWebView;
+public class MetaTexts_Activity extends Base_Activity {
+  @Bind(R.id.metaTextsWebView) WebView metaTextsWebView;
   @Bind(R.id.toolbar) Toolbar toolbar;
 
   @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
 
-    setContentView(R.layout.activity_user_policy);
+    setContentView(R.layout.activity_meta_texts);
 
     ButterKnife.bind(this);
 
-    setupToolbar(toolbar, getResources().getString(R.string.user_policy_activity_title), true);
-
     if (getIntent().getExtras() != null) {
-      String userPolicyString = getIntent().getExtras().getString(Constants.EXTRAS_USER_POLICY);
+      String metaTextString = getIntent().getExtras().getString(Constants.EXTRAS_META_HTML_CONTENT);
 
-      WebSettings settings = userPolicyWebView.getSettings();
+      String activityTitle = getIntent().getExtras().getString(Constants.BUNDLE_ACTIVITY_TITLE);
+
+      setupToolbar(toolbar, activityTitle, true);
+
+      WebSettings settings = metaTextsWebView.getSettings();
       settings.setLoadWithOverviewMode(true);
       settings.setBuiltInZoomControls(true);
       settings.setUseWideViewPort(true);
-      userPolicyWebView.setVerticalScrollBarEnabled(false);
-      userPolicyWebView.setHorizontalScrollBarEnabled(false);
+      metaTextsWebView.setVerticalScrollBarEnabled(false);
+      metaTextsWebView.setHorizontalScrollBarEnabled(false);
 
       //No effect
       settings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
-      userPolicyWebView.setInitialScale(300);
+      metaTextsWebView.setInitialScale(300);
 
       String finalHTMLString = "<!DOCTYPE html>\n" +
           "<html>\n" +
           "<body>\n" +
-          userPolicyString +
+          metaTextString +
           "</body>\n" +
           "</html>\n";
 
-      userPolicyWebView.loadData(finalHTMLString, "text/html", "UTF-8");
+      metaTextsWebView.loadData(finalHTMLString, "text/html", "UTF-8");
     }
   }
 }
