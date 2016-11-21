ccfc22946c9dd4b36e59dfbf750109231b7774b7
oday.maleh <oday.m@mcsaatchi.me>
2016-11-21 16:52:26 +0200

    Add a reminder at every time change of these pickers.

--- app/src/main/java/com/mcsaatchi/gmfit/activities/Reminders_Activity.java
+++ app/src/main/java/com/mcsaatchi/gmfit/activities/Reminders_Activity.java
@@ -7,7 +7,6 @@ import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.RelativeLayout;
 import android.widget.Switch;
-import android.widget.Toast;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import com.codetroopers.betterpickers.radialtimepicker.RadialTimePickerDialogFragment;
@@ -30,47 +29,73 @@ public class Reminders_Activity extends Base_Activity {
 
     setupToolbar(toolbar, getResources().getString(R.string.set_reminders_entry), true);
 
-    enableRemindersSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-      @Override public void onCheckedChanged(CompoundButton compoundButton, boolean checked) {
-        if (checked){
-          Helpers.setupBreakfastAlarm(Reminders_Activity.this, "Breakfast", 4, 9);
-          Helpers.setupBreakfastAlarm(Reminders_Activity.this, "Lunch", 4, 10);
-          Helpers.setupBreakfastAlarm(Reminders_Activity.this, "Dinner", 4, 11);
-        }
-      }
-    });
-
     final RadialTimePickerDialogFragment rtpd =
-        new RadialTimePickerDialogFragment().setOnTimeSetListener(
-            new RadialTimePickerDialogFragment.OnTimeSetListener() {
-              @Override public void onTimeSet(RadialTimePickerDialogFragment dialog, int hourOfDay,
-                  int minute) {
-                Toast.makeText(Reminders_Activity.this,
-                    "Time selected was " + hourOfDay + ":" + minute, Toast.LENGTH_SHORT).show();
-              }
-            })
-            .setForced12hFormat()
+        new RadialTimePickerDialogFragment().setForced12hFormat()
             .setThemeDark()
             .setDoneText(getResources().getString(R.string.ok))
             .setCancelText(getResources().getString(R.string.decline_cancel));
 
+    enableRemindersSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+      @Override public void onCheckedChanged(CompoundButton compoundButton, boolean checked) {
+        if (checked) {
+          Helpers.setupMealRemindersAlarm(Reminders_Activity.this, "Breakfast", 4, 9);
+          Helpers.setupMealRemindersAlarm(Reminders_Activity.this, "Lunch", 4, 10);
+          Helpers.setupMealRemindersAlarm(Reminders_Activity.this, "Dinner", 4, 11);
+        }
+      }
+    });
+
+    /**
+     * BREAKFAST TIMER
+     */
     breakfastReminderLayout.setOnClickListener(new View.OnClickListener() {
       @Override public void onClick(View view) {
-        rtpd.setStartTime(10, 0);
+        rtpd.setOnTimeSetListener(new RadialTimePickerDialogFragment.OnTimeSetListener() {
+          @Override
+          public void onTimeSet(RadialTimePickerDialogFragment dialog, int hourOfDay, int minute) {
+            rtpd.setStartTime(10, 0);
+
+            Helpers.setupMealRemindersAlarm(Reminders_Activity.this, "Breakfast", hourOfDay,
+                minute);
+          }
+        });
+
         rtpd.show(getSupportFragmentManager(), null);
       }
     });
 
+    /**
+     * LUNCH TIMER
+     */
     lunchReminderLayout.setOnClickListener(new View.OnClickListener() {
       @Override public void onClick(View view) {
-        rtpd.setStartTime(2, 45);
+        rtpd.setOnTimeSetListener(new RadialTimePickerDialogFragment.OnTimeSetListener() {
+          @Override
+          public void onTimeSet(RadialTimePickerDialogFragment dialog, int hourOfDay, int minute) {
+            rtpd.setStartTime(2, 45);
+
+            Helpers.setupMealRemindersAlarm(Reminders_Activity.this, "Lunch", hourOfDay, minute);
+          }
+        });
+
         rtpd.show(getSupportFragmentManager(), null);
       }
     });
 
+    /**
+     * DINNER TIMER
+     */
     dinnerReminderLayout.setOnClickListener(new View.OnClickListener() {
       @Override public void onClick(View view) {
-        rtpd.setStartTime(9, 0);
+        rtpd.setOnTimeSetListener(new RadialTimePickerDialogFragment.OnTimeSetListener() {
+          @Override
+          public void onTimeSet(RadialTimePickerDialogFragment dialog, int hourOfDay, int minute) {
+            rtpd.setStartTime(9, 0);
+
+            Helpers.setupMealRemindersAlarm(Reminders_Activity.this, "Dinner", hourOfDay, minute);
+          }
+        });
+
         rtpd.show(getSupportFragmentManager(), null);
       }
     });
