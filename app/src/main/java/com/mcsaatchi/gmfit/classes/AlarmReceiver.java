781f316d178c47e4bbf8dab5967756de8d69f5ca
oday.maleh <oday.m@mcsaatchi.me>
2016-11-21 16:52:54 +0200

    Have a pending intent that opens up the appropriate Add Meal screen for the specific meal type that the reminder was created for

--- app/src/main/java/com/mcsaatchi/gmfit/classes/AlarmReceiver.java
+++ app/src/main/java/com/mcsaatchi/gmfit/classes/AlarmReceiver.java
@@ -10,7 +10,6 @@ import android.net.Uri;
 import android.support.v7.app.NotificationCompat;
 import com.mcsaatchi.gmfit.R;
 import com.mcsaatchi.gmfit.activities.AddNewMealItem_Activity;
-import com.mcsaatchi.gmfit.activities.Reminders_Activity;
 import org.joda.time.LocalDate;
 
 public class AlarmReceiver extends BroadcastReceiver {
@@ -24,17 +23,13 @@ public class AlarmReceiver extends BroadcastReceiver {
 
     String mealType = intent.getExtras().getString("MEAL_TYPE");
 
-    Intent openMealEntryPicker = new Intent(context, AddNewMealItem_Activity.class);
-    openMealEntryPicker.putExtra(Constants.EXTRAS_MAIN_MEAL_NAME, mealType);
-    openMealEntryPicker.putExtra(Constants.EXTRAS_DATE_TO_ADD_MEAL_ON,
+    Intent openMealEntryPickerIntent = new Intent(context, AddNewMealItem_Activity.class);
+    openMealEntryPickerIntent.putExtra(Constants.EXTRAS_MAIN_MEAL_NAME, mealType);
+    openMealEntryPickerIntent.putExtra(Constants.EXTRAS_DATE_TO_ADD_MEAL_ON,
         Helpers.prepareDateForAPIRequest(new LocalDate()));
-    openMealEntryPicker.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-    context.startActivity(openMealEntryPicker);
+    openMealEntryPickerIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 
-    Intent notificationIntent = new Intent(context, Reminders_Activity.class);
-    notificationIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-
-    PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, notificationIntent,
+    PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, openMealEntryPickerIntent,
         PendingIntent.FLAG_UPDATE_CURRENT);
 
     Uri alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
