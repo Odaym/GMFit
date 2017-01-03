be901d1ff1f0d74ca11731188055fc5800cc19fc
oday.maleh <oday.m@mcsaatchi.me>
2017-01-03 16:51:35 +0200

    Add the click listener on the medication results list so that it opens the Add Existing Medication screen

--- app/src/main/java/com/mcsaatchi/gmfit/health/adapters/SearchMedicationsRecyclerAdapter.java
+++ app/src/main/java/com/mcsaatchi/gmfit/health/adapters/SearchMedicationsRecyclerAdapter.java
@@ -1,12 +1,15 @@
 package com.mcsaatchi.gmfit.health.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 import com.mcsaatchi.gmfit.R;
+import com.mcsaatchi.gmfit.common.Constants;
+import com.mcsaatchi.gmfit.health.activities.AddExistingMedicationActivity;
 import com.mcsaatchi.gmfit.health.models.Medication;
 import java.util.List;
 
@@ -46,6 +49,15 @@ public class SearchMedicationsRecyclerAdapter extends RecyclerView.Adapter {
 
     public ViewHolder(View itemView) {
       super(itemView);
+      itemView.setOnClickListener(new View.OnClickListener() {
+        @Override public void onClick(View view) {
+          Intent intent = new Intent(context, AddExistingMedicationActivity.class);
+          intent.putExtra(Constants.EXTRAS_MEDICATION_ITEM,
+              medicationsList.get(getAdapterPosition()));
+          context.startActivity(intent);
+        }
+      });
+      
       medicineNameTV = (TextView) itemView.findViewById(R.id.medicineNameTV);
       medicineDescriptionTV = (TextView) itemView.findViewById(R.id.medicineDescriptionTV);
     }
