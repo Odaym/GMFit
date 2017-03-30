faa933520468e28b8fe3af0a0983182ca1dec34c
oday.maleh <oday.m@mcsaatchi.me>
2017-03-30 16:35:30 +0300

    Update Password for Insurance MVP + fix to not updating

--- app/src/main/java/com/mcsaatchi/gmfit/insurance/activities/home/UpdatePasswordActivity.java
+++ app/src/main/java/com/mcsaatchi/gmfit/insurance/activities/home/UpdatePasswordActivity.java
@@ -1,10 +1,8 @@
 package com.mcsaatchi.gmfit.insurance.activities.home;
 
-import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.Toolbar;
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -12,18 +10,15 @@ import butterknife.OnClick;
 import com.andreabaccega.widget.FormEditText;
 import com.mcsaatchi.gmfit.R;
 import com.mcsaatchi.gmfit.architecture.rest.InsuranceLoginResponseInnerData;
-import com.mcsaatchi.gmfit.architecture.rest.UpdateInsurancePasswordResponse;
 import com.mcsaatchi.gmfit.common.Constants;
 import com.mcsaatchi.gmfit.common.activities.BaseActivity;
 import com.mcsaatchi.gmfit.common.classes.Helpers;
+import com.mcsaatchi.gmfit.insurance.activities.presenters.UpdatePasswordActivityPresenter;
 import com.mcsaatchi.gmfit.insurance.fragments.InsuranceLoginFragment;
 import java.util.ArrayList;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import timber.log.Timber;
 
-public class UpdatePasswordActivity extends BaseActivity {
+public class UpdatePasswordActivity extends BaseActivity
+    implements UpdatePasswordActivityPresenter.UpdatePasswordActivityView {
 
   @Bind(R.id.toolbar) Toolbar toolbar;
   @Bind(R.id.newPasswordET) FormEditText newPasswordET;
@@ -32,10 +27,9 @@ public class UpdatePasswordActivity extends BaseActivity {
   @Bind(R.id.phoneNumberET) FormEditText phoneNumberET;
 
   private String old_password;
-
   private ArrayList<FormEditText> allFields = new ArrayList<>();
-
   private InsuranceLoginResponseInnerData insuranceUserData;
+  private UpdatePasswordActivityPresenter presenter;
 
   @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -47,8 +41,12 @@ public class UpdatePasswordActivity extends BaseActivity {
     setupToolbar(getClass().getSimpleName(), toolbar,
         getString(R.string.insurance_update_password_activity_title), true);
 
+    presenter = new UpdatePasswordActivityPresenter(this, dataAccessHandler);
+
     allFields.add(newPasswordET);
     allFields.add(retypePasswordET);
+    allFields.add(emailAddressET);
+    allFields.add(phoneNumberET);
 
     if (getIntent().getExtras() != null) {
       insuranceUserData = (InsuranceLoginResponseInnerData) getIntent().getExtras()
@@ -59,51 +57,18 @@ public class UpdatePasswordActivity extends BaseActivity {
 
   @OnClick(R.id.updateInfoBTN) public void handleUpdateInfo() {
     if (Helpers.validateFields(allFields)) {
-      updateInsurancePassword(String.valueOf(insuranceUserData.getContracts().get(0).getNumber()),
-          old_password, newPasswordET.getText().toString(), insuranceUserData.getEmail(),
-          insuranceUserData.getMobile());
+      presenter.updateInsurancePassword(
+          String.valueOf(insuranceUserData.getContracts().get(0).getNumber()), old_password,
+          newPasswordET.getText().toString(), emailAddressET.getText().toString(),
+          phoneNumberET.getText().toString());
     }
   }
 
-  private void updateInsurancePassword(String contractNumber, String oldPassword,
-      final String newPassword, String email, String mobileNumber) {
-    final ProgressDialog waitingDialog = new ProgressDialog(this);
-    waitingDialog.setTitle(getResources().getString(R.string.loading_data_dialog_title));
-    waitingDialog.setMessage(getResources().getString(R.string.please_wait_dialog_message));
-    waitingDialog.setCancelable(false);
-    waitingDialog.show();
-
-    final AlertDialog alertDialog = new AlertDialog.Builder(this).create();
-    alertDialog.setTitle(R.string.updating_password_dialog_title);
-    alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, getResources().getString(R.string.ok),
-        (dialog, which) -> {
-          dialog.dismiss();
-
-          if (waitingDialog.isShowing()) waitingDialog.dismiss();
-        });
-
-    dataAccessHandler.updateInsurancePassword(contractNumber, oldPassword, newPassword, email,
-        mobileNumber, new Callback<UpdateInsurancePasswordResponse>() {
-          @Override public void onResponse(Call<UpdateInsurancePasswordResponse> call,
-              Response<UpdateInsurancePasswordResponse> response) {
-
-            switch (response.code()) {
-              case 200:
-                waitingDialog.dismiss();
-
-                Intent intent = new Intent();
-                intent.putExtra(Constants.BUNDLE_INSURANCE_USER_OBJECT, insuranceUserData);
-                intent.putExtra(Constants.EXTRAS_INSURANCE_USER_PASSWORD, newPassword);
-                setResult(InsuranceLoginFragment.INFO_UPDATED_SUCCESSFULLY_AFTER_LOGIN, intent);
-                finish();
-            }
-          }
-
-          @Override public void onFailure(Call<UpdateInsurancePasswordResponse> call, Throwable t) {
-            Timber.d("Call failed with error : %s", t.getMessage());
-            alertDialog.setMessage(getString(R.string.server_error_got_returned));
-            alertDialog.show();
-          }
-        });
+  @Override public void sendResultBackAndCloseActivity(String newPassword) {
+    Intent intent = new Intent();
+    intent.putExtra(Constants.BUNDLE_INSURANCE_USER_OBJECT, insuranceUserData);
+    intent.putExtra(Constants.EXTRAS_INSURANCE_USER_PASSWORD, newPassword);
+    setResult(InsuranceLoginFragment.INFO_UPDATED_SUCCESSFULLY_AFTER_LOGIN, intent);
+    finish();
   }
 }
---
+++ app/src/main/java/com/mcsaatchi/gmfit/insurance/activities/presenters/UpdatePasswordActivityPresenter.java
@@ -0,0 +1,46 @@
+package com.mcsaatchi.gmfit.insurance.activities.presenters;
+
+import com.mcsaatchi.gmfit.R;
+import com.mcsaatchi.gmfit.architecture.data_access.DataAccessHandler;
+import com.mcsaatchi.gmfit.architecture.rest.UpdateInsurancePasswordResponse;
+import com.mcsaatchi.gmfit.common.presenters.BaseActivityPresenter;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class UpdatePasswordActivityPresenter extends BaseActivityPresenter {
+  private UpdatePasswordActivityView view;
+  private DataAccessHandler dataAccessHandler;
+
+  public UpdatePasswordActivityPresenter(UpdatePasswordActivityView view,
+      DataAccessHandler dataAccessHandler) {
+    this.view = view;
+    this.dataAccessHandler = dataAccessHandler;
+  }
+
+  public void updateInsurancePassword(String contractNumber, String oldPassword,
+      final String newPassword, String email, String mobileNumber) {
+
+    view.callDisplayWaitingDialog(R.string.loading_data_dialog_title);
+
+    dataAccessHandler.updateInsurancePassword(contractNumber, oldPassword, newPassword, email,
+        mobileNumber, new Callback<UpdateInsurancePasswordResponse>() {
+          @Override public void onResponse(Call<UpdateInsurancePasswordResponse> call,
+              Response<UpdateInsurancePasswordResponse> response) {
+            switch (response.code()) {
+              case 200:
+                view.sendResultBackAndCloseActivity(newPassword);
+                break;
+            }
+          }
+
+          @Override public void onFailure(Call<UpdateInsurancePasswordResponse> call, Throwable t) {
+            view.displayRequestErrorDialog(t.getMessage());
+          }
+        });
+  }
+
+  public interface UpdatePasswordActivityView extends BaseActivityView {
+    void sendResultBackAndCloseActivity(String newPassword);
+  }
+}
